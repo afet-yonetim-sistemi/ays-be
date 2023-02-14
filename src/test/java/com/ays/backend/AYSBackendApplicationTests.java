8304f464058f666656ea632585de27239cb32b37
Halil Turkoglu <halilturkoglucs@gmail.com>
2023-02-14 22:24:23 +0100

    Spring skeleton for initial use (#3)
    * Spring    skeleton for initial use
    
    * `application.properties` has been changed as `application.yml` file
    
    ---------
    
    Co-authored-by: Agit Rubar Demir <demiragitrubar@gmail.com>

---
+++ src/test/java/com/ays/backend/AYSBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ays.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AYSBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
