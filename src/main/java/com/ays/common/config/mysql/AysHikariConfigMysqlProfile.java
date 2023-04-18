e449c31a1bc398339d4ea5f9e291fbe1df9f8d0e
skayikci <serhat.kayikci@gmail.com>
2023-04-18 23:19:17 +0200

    latest updates
    latest updates regarding the env settings for docker compose.

--- src/main/java/com/ays/common/config/mysql/AysHikariConfigMysqlProfile.java
+++ src/main/java/com/ays/common/config/mysql/AysHikariConfigMysqlProfile.java
@@ -2,10 +2,8 @@ package com.ays.common.config.mysql;
 
 import com.zaxxer.hikari.HikariConfig;
 import lombok.Builder;
-import lombok.extern.slf4j.Slf4j;
 
 @Builder
-@Slf4j
 public class AysHikariConfigMysqlProfile {
 
     private final String username;
@@ -20,9 +18,6 @@ public class AysHikariConfigMysqlProfile {
         hikariConfig.setUsername(this.username);
         hikariConfig.setPassword(this.password);
         hikariConfig.setJdbcUrl(this.url);
-        log.info(this.url);
-        log.info(this.username);
-        log.info(this.password);
         hikariConfig.setMaximumPoolSize(Integer.parseInt(this.maximumPoolSize));
         hikariConfig.setConnectionTimeout(Long.parseLong(this.connectionTimeout));
         hikariConfig.setMaxLifetime(Long.parseLong(this.maximumLifetime));
