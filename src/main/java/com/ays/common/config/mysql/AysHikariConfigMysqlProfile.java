b347f5df605c85a489dbc459f7fbb873c1564ba8
skayikci <serhat.kayikci@gmail.com>
2023-04-18 22:45:57 +0200

    Update AysHikariConfigMysqlProfile.java
    add logging, change driver for hikaripool

--- src/main/java/com/ays/common/config/mysql/AysHikariConfigMysqlProfile.java
+++ src/main/java/com/ays/common/config/mysql/AysHikariConfigMysqlProfile.java
@@ -2,8 +2,10 @@ package com.ays.common.config.mysql;
 
 import com.zaxxer.hikari.HikariConfig;
 import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 
 @Builder
+@Slf4j
 public class AysHikariConfigMysqlProfile {
 
     private final String username;
@@ -18,10 +20,13 @@ public class AysHikariConfigMysqlProfile {
         hikariConfig.setUsername(this.username);
         hikariConfig.setPassword(this.password);
         hikariConfig.setJdbcUrl(this.url);
+        log.info(this.url);
+        log.info(this.username);
+        log.info(this.password);
         hikariConfig.setMaximumPoolSize(Integer.parseInt(this.maximumPoolSize));
         hikariConfig.setConnectionTimeout(Long.parseLong(this.connectionTimeout));
         hikariConfig.setMaxLifetime(Long.parseLong(this.maximumLifetime));
-        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        hikariConfig.setDriverClassName("software.aws.rds.jdbc.mysql.Driver");
         hikariConfig.setAutoCommit(true);
         return hikariConfig;
     }
