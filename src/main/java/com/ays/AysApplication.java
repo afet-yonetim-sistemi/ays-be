5056d65d5f7302b11aec4d5fdabab6bbe1aefa67
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-04-26 20:57:58 +0300

    JavaDOC Has Been Added to `AysApplication.java` Class

--- src/main/java/com/ays/AysApplication.java
+++ src/main/java/com/ays/AysApplication.java
@@ -3,6 +3,16 @@ package com.ays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class for the Ays Application.
+ * <p>This class is annotated with {@code @SpringBootApplication}, which combines three other annotations:
+ * {@code @Configuration}, {@code @EnableAutoConfiguration}, and {@code @ComponentScan}.
+ * <p>The {@code main} method is used to start the Spring Boot application by calling {@code SpringApplication.run}.
+ *
+ * @see org.springframework.boot.autoconfigure.SpringBootApplication
+ * @see org.springframework.context.annotation.Configuration
+ * @see org.springframework.boot.SpringApplication
+ */
 @SpringBootApplication
 class AysApplication {
     public static void main(String[] args) {
