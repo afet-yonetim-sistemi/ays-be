90f4d845eac8489be777e42ed28f3b5d5343e3ba
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-04-26 20:56:30 +0300

    `AysApplicationEventListener.java` Has Been Added to Project

--- src/main/java/com/ays/AysApplication.java
+++ src/main/java/com/ays/AysApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AysApplication {
+class AysApplication {
     public static void main(String[] args) {
         SpringApplication.run(AysApplication.class, args);
     }
