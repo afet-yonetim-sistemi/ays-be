8ce17328b9015beb036c8b84f39746d5275f6f33
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-04-27 19:16:59 +0300

    Add comments for validation information in AdminUserRegisterRequest

--- src/main/java/com/ays/admin_user/model/dto/request/AdminUserRegisterRequest.java
+++ src/main/java/com/ays/admin_user/model/dto/request/AdminUserRegisterRequest.java
@@ -10,27 +10,35 @@ import lombok.Data;
 @Builder
 public class AdminUserRegisterRequest {
 
+    /* verificationId should be not null and its trimmed length should be greather than zero */
     @NotBlank
     private String verificationId;
 
+    /* organizationId should be not null and its trimmed length should be greather than zero */
     @NotBlank
     private String organizationId;
 
+    /* username should be not null and its trimmed length should be greather than zero */
     @NotBlank
     private String username;
 
+    /* email should be not null and its trimmed length should be greather than zero */
+    /* email should be valid */
     @NotBlank
     @Email
     private String email;
 
+    /* password should be not null and its trimmed length should be greather than zero */
     @NotBlank
     private String password;
 
     private AysPhoneNumber phoneNumber;
 
+    /* firstName should be not null and its trimmed length should be greather than zero */
     @NotBlank
     private String firstName;
 
+    /* lastName should be not null and its trimmed length should be greather than zero */
     @NotBlank
     private String lastName;
 
