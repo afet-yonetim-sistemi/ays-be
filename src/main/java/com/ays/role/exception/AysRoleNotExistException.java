f7d16026610fb111d1611484220cbd60b07b1c2c
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-04-30 16:34:04 +0300

    Java DOC Has Been Added for AysRoleNotExistException Class

--- src/main/java/com/ays/role/exception/AysRoleNotExistException.java
+++ src/main/java/com/ays/role/exception/AysRoleNotExistException.java
@@ -2,8 +2,24 @@ package com.ays.role.exception;
 
 import com.ays.common.util.exception.AysNotExistException;
 
+import java.io.Serial;
+
+/**
+ * Exception to be thrown when a requested role does not exist.
+ */
 public class AysRoleNotExistException extends AysNotExistException {
 
+    /**
+     * Unique serial version ID.
+     */
+    @Serial
+    private static final long serialVersionUID = 6116668401680664562L;
+
+    /**
+     * Constructor to create a new AysRoleNotExistException instance with the provided message.
+     *
+     * @param message the detail message.
+     */
     public AysRoleNotExistException(String message) {
         super(message);
     }
