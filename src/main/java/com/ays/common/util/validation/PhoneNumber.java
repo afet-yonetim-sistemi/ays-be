99637e0220ac917a14131e1b6677511c900c7cc6
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-04-30 15:50:05 +0300

    Java DOC Has Been Added for PhoneNumber Validator Classes

--- src/main/java/com/ays/common/util/validation/PhoneNumber.java
+++ src/main/java/com/ays/common/util/validation/PhoneNumber.java
@@ -8,15 +8,33 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to validate phone numbers using {@link PhoneNumberValidator}.
+ */
 @Target(value = ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = PhoneNumberValidator.class)
 public @interface PhoneNumber {
 
+    /**
+     * Returns the error message when phone number is not valid.
+     *
+     * @return the error message
+     */
     String message() default "Phone number is not valid!";
 
+    /**
+     * Returns the validation groups to which this constraint belongs.
+     *
+     * @return the validation groups
+     */
     Class<?>[] groups() default {};
 
+    /**
+     * Returns the payload associated to this constraint.
+     *
+     * @return the payload
+     */
     Class<? extends Payload>[] payload() default {};
 
 }
--- src/main/java/com/ays/common/util/validation/PhoneNumberValidator.java
+++ src/main/java/com/ays/common/util/validation/PhoneNumberValidator.java
@@ -4,8 +4,21 @@ import com.ays.common.model.AysPhoneNumber;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+/**
+ * A custom validator implementation for the {@link PhoneNumber} annotation.
+ * Validates whether the provided {@link AysPhoneNumber} object is a valid phone number or not based on the
+ * country code and line number length.
+ */
 class PhoneNumberValidator implements ConstraintValidator<PhoneNumber, AysPhoneNumber> {
 
+    /**
+     * Validates whether the provided {@link AysPhoneNumber} object is a valid phone number or not based on the
+     * country code and line number length.
+     *
+     * @param phoneNumber the {@link AysPhoneNumber} object to be validated
+     * @param context     the context in which the constraint is evaluated
+     * @return true if the phone number is valid, false otherwise
+     */
     @Override
     public boolean isValid(AysPhoneNumber phoneNumber, ConstraintValidatorContext context) {
         final var countryCodeLength = String.valueOf(phoneNumber.getLineNumber()).length();
