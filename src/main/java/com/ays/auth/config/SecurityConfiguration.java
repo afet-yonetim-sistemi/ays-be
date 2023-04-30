394d7b077c8f94e7a46405facd4d3bf5b136c723
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-04-30 19:17:47 +0300

    `@EnableMethodSecurity` Annotation Has Been Added to SecurityConfiguration Class for Using `@PreAuthorize` Annotation

--- src/main/java/com/ays/auth/config/SecurityConfiguration.java
+++ src/main/java/com/ays/auth/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,6 +36,7 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalAuthentication
+@EnableMethodSecurity
 @RequiredArgsConstructor
 class SecurityConfiguration {
 
