c4907caac97eb7bb3ffc6956793f4c4843db6495
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-04-30 15:02:44 +0300

    Java DOCs Have Been Added to AYS Admin User Exceptions

--- src/main/java/com/ays/admin_user/util/exception/AysAdminUserAlreadyExistsByUsernameException.java
+++ src/main/java/com/ays/admin_user/util/exception/AysAdminUserAlreadyExistsByUsernameException.java
@@ -2,11 +2,25 @@ package com.ays.admin_user.util.exception;
 
 import com.ays.common.util.exception.AysAlreadyException;
 
+import java.io.Serial;
+
 /**
- * Exception class defining fired when a admin user already exists in the database.
+ * Exception indicating that an admin user already exists with the specified username.
+ * This exception is a subclass of AysAlreadyException.
  */
 public class AysAdminUserAlreadyExistsByUsernameException extends AysAlreadyException {
 
+    /**
+     * Unique identifier for serialization.
+     */
+    @Serial
+    private static final long serialVersionUID = -3186201226632256358L;
+
+    /**
+     * Constructs a new AysAdminUserAlreadyExistsByUsernameException with the specified username.
+     *
+     * @param username The username of the admin user that already exists.
+     */
     public AysAdminUserAlreadyExistsByUsernameException(String username) {
         super("ADMIN USER ALREADY EXIST! username:" + username);
     }
--- src/main/java/com/ays/admin_user/util/exception/AysAdminUserNotActiveException.java
+++ src/main/java/com/ays/admin_user/util/exception/AysAdminUserNotActiveException.java
@@ -1,7 +1,27 @@
 package com.ays.admin_user.util.exception;
 
+import java.io.Serial;
+
+/**
+ * Exception indicating that an admin user is not active and cannot perform the requested action.
+ * This exception is a subclass of RuntimeException, which means it is an unchecked exception and does not need to
+ * be declared in a throws clause or caught explicitly.
+ * Typically, this exception is thrown when an admin user tries to perform an action that is only allowed for active
+ * users, but the user is currently inactive.
+ */
 public class AysAdminUserNotActiveException extends RuntimeException {
 
+    /**
+     * Unique identifier for serialization.
+     */
+    @Serial
+    private static final long serialVersionUID = 1008678165854009528L;
+
+    /**
+     * Constructs a new AysAdminUserNotActiveException with the specified username.
+     *
+     * @param username The username of the inactive admin user.
+     */
     public AysAdminUserNotActiveException(String username) {
         super("USER IS NOT ACTIVE! username:{}" + username);
     }
