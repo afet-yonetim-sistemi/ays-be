da3317b6c979a278e429cae9c9a7cb531c282050
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-05-07 10:23:20 +0300

    Bean Scope Utility Class Has Been Created

---
+++ src/main/java/com/ays/common/model/enums/BeanScope.java
@@ -0,0 +1,21 @@
+package com.ays.common.model.enums;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * This utility class is used to define the bean scope values in the Spring Framework.
+ */
+@UtilityClass
+public class BeanScope {
+
+    /**
+     * Bean instances defined with "prototype" scope will create a new instance for each request.
+     */
+    public static final String SCOPE_PROTOTYPE = "prototype";
+
+    /**
+     * Bean instances defined with "request" scope will create an instance that is available for the duration of an HTTP request.
+     */
+    public static final String SCOPE_REQUEST = "request";
+
+}
