e31f4c3535bff23cb6c33188fb783f857aaf72dc
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-05-08 22:52:20 +0300

    Add more Java Doc for UserUpdateRequest

--- src/main/java/com/ays/user/model/dto/request/UserUpdateRequest.java
+++ src/main/java/com/ays/user/model/dto/request/UserUpdateRequest.java
@@ -8,6 +8,13 @@ import lombok.Data;
 
 /**
  * Represents a request object for updating user with its variables.
+ * <p>
+ * This class provides getters and setters for the id, organization id, username, first name,
+ * last name, email, user role, user status, country code and line number fields.
+ * It also includes a builder pattern implementation for constructing instances of this class with optional parameters.
+ * <p>
+ * The purpose of this class is to encapsulate the request data related to updating a user, allowing for easy
+ * transfer of the data between different layers of the application.
  */
 @Builder
 @Data
