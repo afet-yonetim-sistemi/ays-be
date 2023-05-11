c544f8766dbe7f0a3f60b857cb4ce6a3c82ce7b9
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-05-11 21:08:11 +0300

    `withRole()` and `withStatus()` Methods Have Been Added to `UserUpdateRequestBuilder` Class

--- src/test/java/com/ays/user/model/dto/request/UserUpdateRequestBuilder.java
+++ src/test/java/com/ays/user/model/dto/request/UserUpdateRequestBuilder.java
@@ -1,11 +1,23 @@
 package com.ays.user.model.dto.request;
 
 import com.ays.common.model.TestDataBuilder;
+import com.ays.user.model.enums.UserRole;
+import com.ays.user.model.enums.UserStatus;
 
 public class UserUpdateRequestBuilder extends TestDataBuilder<UserUpdateRequest> {
 
     public UserUpdateRequestBuilder() {
         super(UserUpdateRequest.class);
+    }
+
+    public UserUpdateRequestBuilder withRole(UserRole role) {
+        data.setRole(role);
+        return this;
+    }
+
+    public UserUpdateRequestBuilder withStatus(UserStatus status) {
+        data.setStatus(status);
+        return this;
     }
 
 }
