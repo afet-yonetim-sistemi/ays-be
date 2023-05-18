3f1cfe943bac39ec86ea56beb6433b165496382d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-05-19 01:46:32 +0300

    Add Java Doc for AdminUserSpecifications and SearchSpecificationBuilder

--- src/main/java/com/ays/admin_user/repository/specification/AdminUserSpecifications.java
+++ src/main/java/com/ays/admin_user/repository/specification/AdminUserSpecifications.java
@@ -3,8 +3,16 @@ package com.ays.admin_user.repository.specification;
 import com.ays.admin_user.model.entity.AdminUserEntity;
 import org.springframework.data.jpa.domain.Specification;
 
+/**
+ * AdminUserSpecifications for handling with getting all list with respect to the defined criteria
+ */
 public class AdminUserSpecifications {
 
+    /**
+     * hasOrganizationId method for getting all admin list by organization Id
+     *
+     * @param organizationId the organization identifier
+     */
     public static Specification<AdminUserEntity> hasOrganizationId(String organizationId) {
         return (root, query, criteriaBuilder) ->
                 SearchSpecificationBuilder.eq(criteriaBuilder, root.get("organizationId"), organizationId);
--- src/main/java/com/ays/admin_user/repository/specification/SearchSpecificationBuilder.java
+++ src/main/java/com/ays/admin_user/repository/specification/SearchSpecificationBuilder.java
@@ -6,9 +6,14 @@ import jakarta.persistence.criteria.Predicate;
 
 import java.util.Locale;
 
-
+/**
+ * SearchSpecificationBuilder for handling with Specification operations
+ */
 public class SearchSpecificationBuilder {
 
+    /**
+     * or operation for or specification
+     */
     public static Predicate or(CriteriaBuilder criteriaBuilder, Predicate predicate1, Predicate predicate2) {
         if (predicate2 == null && predicate1 == null) {
             return null;
@@ -23,6 +28,9 @@ public class SearchSpecificationBuilder {
         return criteriaBuilder.or(predicate1, predicate2);
     }
 
+    /**
+     * and operation for and specification
+     */
     public static Predicate and(CriteriaBuilder criteriaBuilder, Predicate predicate1, Predicate predicate2) {
         if (predicate2 == null && predicate1 == null) {
             return null;
@@ -37,6 +45,9 @@ public class SearchSpecificationBuilder {
         return criteriaBuilder.and(predicate1, predicate2);
     }
 
+    /**
+     * like operation for like specification
+     */
     public static Predicate like(CriteriaBuilder builder, Path<String> path, String value) {
         if (value == null) {
             return null;
@@ -54,6 +65,9 @@ public class SearchSpecificationBuilder {
         return builder.like(builder.lower(path), "%" + value + "%");
     }
 
+    /**
+     * eq operation for equal specification
+     */
     public static Predicate eq(CriteriaBuilder criteriaBuilder, Path<Object> path, Object value) {
         if (value == null) {
             return null;
@@ -61,6 +75,9 @@ public class SearchSpecificationBuilder {
         return criteriaBuilder.equal(path, value);
     }
 
+    /**
+     * ne operation for not equal specification
+     */
     public static Predicate ne(CriteriaBuilder criteriaBuilder, Path<Object> path, Object value) {
         if (value == null) {
             return null;
@@ -69,14 +86,23 @@ public class SearchSpecificationBuilder {
     }
 
 
+    /**
+     * isNull operation for is null specification
+     */
     public static Predicate isNull(CriteriaBuilder criteriaBuilder, Path<Object> path) {
         return criteriaBuilder.isNull(path);
     }
 
+    /**
+     * isNotNull operation for is not null specification
+     */
     public static Predicate isNotNull(CriteriaBuilder criteriaBuilder, Path<Object> path) {
         return criteriaBuilder.isNotNull(path);
     }
 
+    /**
+     * in operation for in specification
+     */
     public static Predicate in(CriteriaBuilder criteriaBuilder, Path<Object> path, Object[] value) {
         if (value == null) {
             return null;
@@ -84,6 +110,9 @@ public class SearchSpecificationBuilder {
         return criteriaBuilder.in(path).value(value);
     }
 
+    /**
+     * notIn operation for not in specification
+     */
     public static Predicate notIn(CriteriaBuilder criteriaBuilder, Path<Object> path, Object[] value) {
         if (value == null) {
             return null;
