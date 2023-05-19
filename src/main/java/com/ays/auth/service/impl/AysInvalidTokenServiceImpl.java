80a3d17285d6f5b32e95f35f69245acd77e1087b
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-05-19 10:56:18 +0300

    Java DOC Has Been Added to `AysInvalidTokenService` Layer

--- src/main/java/com/ays/auth/service/impl/AysInvalidTokenServiceImpl.java
+++ src/main/java/com/ays/auth/service/impl/AysInvalidTokenServiceImpl.java
@@ -10,12 +10,21 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * AysInvalidTokenServiceImpl is an implementation of the AysInvalidTokenService interface that provides
+ * functionality for managing and checking the validity of tokens.
+ */
 @Service
 @RequiredArgsConstructor
 class AysInvalidTokenServiceImpl implements AysInvalidTokenService {
 
     private final AysInvalidTokenRepository invalidTokenRepository;
 
+    /**
+     * Invalidates the specified token IDs by saving them as invalid tokens in the repository.
+     *
+     * @param tokenIds a set of token IDs to invalidate
+     */
     @Override
     public void invalidateTokens(final Set<String> tokenIds) {
         final Set<AysInvalidTokenEntity> invalidTokenEntities = tokenIds.stream()
@@ -28,6 +37,13 @@ class AysInvalidTokenServiceImpl implements AysInvalidTokenService {
         invalidTokenRepository.saveAll(invalidTokenEntities);
     }
 
+    /**
+     * Checks the validity of a token by searching for it in the repository.
+     * If the token is found and marked as invalid, a TokenAlreadyInvalidatedException is thrown.
+     *
+     * @param tokenId the ID of the token to check for invalidity
+     * @throws TokenAlreadyInvalidatedException if the token is already marked as invalid
+     */
     @Override
     public void checkForInvalidityOfToken(final String tokenId) {
         final boolean isTokenInvalid = invalidTokenRepository.findByTokenId(tokenId).isPresent();
