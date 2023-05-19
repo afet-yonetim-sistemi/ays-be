b43c7febab59f5655dff78255f33c4f5bfca41d2
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-05-19 11:02:58 +0300

    Java DOC Has Been Added to `AdminUserAuthController.invalidateTokens()` Method

--- src/main/java/com/ays/admin_user/controller/AdminUserAuthController.java
+++ src/main/java/com/ays/admin_user/controller/AdminUserAuthController.java
@@ -76,6 +76,13 @@ class AdminUserAuthController {
         return AysResponse.successOf(aysTokenResponse);
     }
 
+    /**
+     * Endpoint for invalidating tokens. Only users with the 'ADMIN' authority are allowed to access this endpoint.
+     * It invalidates the access token and refresh token associated with the provided refresh token.
+     *
+     * @param invalidateRequest the request object containing the refresh token to invalidate
+     * @return AysResponse with a success message and no data
+     */
     @PostMapping("/token/invalidate")
     @PreAuthorize("hasAnyAuthority('ADMIN')")
     public AysResponse<Void> invalidateTokens(@RequestBody @Valid AysTokenInvalidateRequest invalidateRequest) {
