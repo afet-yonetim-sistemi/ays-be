27d7e9030d78c13fb2e6bd284a987e070e06a236
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-05-19 10:54:07 +0300

    `UserAuthService.invalidateTokens()` Method Has Been Created

--- src/main/java/com/ays/user/service/impl/UserAuthServiceImpl.java
+++ src/main/java/com/ays/user/service/impl/UserAuthServiceImpl.java
@@ -1,8 +1,10 @@
 package com.ays.user.service.impl;
 
+import com.ays.auth.model.AysIdentity;
 import com.ays.auth.model.AysToken;
 import com.ays.auth.model.dto.request.AysLoginRequest;
 import com.ays.auth.model.enums.AysTokenClaims;
+import com.ays.auth.service.AysInvalidTokenService;
 import com.ays.auth.service.AysTokenService;
 import com.ays.auth.util.exception.PasswordNotValidException;
 import com.ays.auth.util.exception.UserNotActiveException;
@@ -13,20 +15,22 @@ import com.ays.user.service.UserAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 class UserAuthServiceImpl implements UserAuthService {
 
     private final UserRepository userRepository;
 
     private final AysTokenService tokenService;
+    private final AysInvalidTokenService tokenInvalidateService;
     private final PasswordEncoder passwordEncoder;
 
+    private final AysIdentity identity;
+
     @Override
-    @Transactional
     public AysToken authenticate(AysLoginRequest loginRequest) {
 
         final UserEntity userEntity = this.findUser(loginRequest.getUsername());
@@ -62,4 +66,22 @@ class UserAuthServiceImpl implements UserAuthService {
 
         return userEntity;
     }
+
+
+    @Override
+    public void invalidateTokens(final String refreshToken) {
+
+        tokenService.verifyAndValidate(refreshToken);
+
+        final String accessTokenId = tokenService.getClaims(identity.getAccessToken())
+                .get(AysTokenClaims.JWT_ID.getValue()).toString();
+        tokenInvalidateService.checkForInvalidityOfToken(accessTokenId);
+
+        final String refreshTokenId = tokenService.getClaims(refreshToken)
+                .get(AysTokenClaims.JWT_ID.getValue()).toString();
+        tokenInvalidateService.checkForInvalidityOfToken(refreshTokenId);
+
+        tokenInvalidateService.invalidateTokens(Set.of(accessTokenId, refreshTokenId));
+    }
+
 }
