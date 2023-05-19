13664589e5ae06e1f6af6de7389e6cae1b92c394
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-05-19 11:02:33 +0300

    Java DOC Has Been Added to `TokenAlreadyInvalidatedException` Class

--- src/main/java/com/ays/auth/util/exception/TokenAlreadyInvalidatedException.java
+++ src/main/java/com/ays/auth/util/exception/TokenAlreadyInvalidatedException.java
@@ -4,6 +4,11 @@ import com.ays.common.util.exception.AysAuthException;
 
 import java.io.Serial;
 
+/**
+ * TokenAlreadyInvalidatedException is an exception class that is thrown when attempting to invalidate a token
+ * that is already marked as invalid.
+ * It extends the AysAuthException class and provides a specific error message indicating the already invalidated token.
+ */
 public class TokenAlreadyInvalidatedException extends AysAuthException {
 
     /**
@@ -12,6 +17,11 @@ public class TokenAlreadyInvalidatedException extends AysAuthException {
     @Serial
     private static final long serialVersionUID = 926590327251528609L;
 
+    /**
+     * Constructs a new TokenAlreadyInvalidatedException with the given token ID.
+     *
+     * @param tokenId the ID of the token that is already invalidated
+     */
     public TokenAlreadyInvalidatedException(String tokenId) {
         super("TOKEN IS ALREADY INVALIDATED! tokenId: " + tokenId);
     }
