f43a1368cb3c3be979d52b0b535c5228891eaeb6
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-05-19 11:03:39 +0300

    Java DOC Has Been Added to `AysInvalidTokenRepository` Layer

--- src/main/java/com/ays/auth/repository/AysInvalidTokenRepository.java
+++ src/main/java/com/ays/auth/repository/AysInvalidTokenRepository.java
@@ -5,8 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * AysInvalidTokenRepository is a repository interface that provides methods for accessing and managing invalid token entities.
+ */
 public interface AysInvalidTokenRepository extends JpaRepository<AysInvalidTokenEntity, Long> {
 
+    /**
+     * Finds an invalid token entity by its token ID.
+     *
+     * @param tokenId the ID of the token to search for
+     * @return an Optional containing the found invalid token entity, or an empty Optional if not found
+     */
     Optional<AysInvalidTokenEntity> findByTokenId(String tokenId);
 
 }
