ac709d1f955e1739eefc1b30463bd542acc7bd46
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-05-19 10:52:42 +0300

    `AysTokenInvalidateRequest` Class Has Been Created

---
+++ src/main/java/com/ays/auth/model/dto/request/AysTokenInvalidateRequest.java
@@ -0,0 +1,18 @@
+package com.ays.auth.model.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AysTokenInvalidateRequest {
+
+    @NotBlank
+    private String refreshToken;
+
+}
