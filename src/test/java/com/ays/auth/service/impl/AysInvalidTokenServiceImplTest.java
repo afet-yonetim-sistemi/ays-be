1246c63191575d6f6809600194ac00af45b4256a
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-05-20 23:00:51 +0300

    `AysInvalidTokenService.checkForInvalidityOfToken()` Method Has Been Covered with Unit Tests

--- src/test/java/com/ays/auth/service/impl/AysInvalidTokenServiceImplTest.java
+++ src/test/java/com/ays/auth/service/impl/AysInvalidTokenServiceImplTest.java
@@ -2,13 +2,17 @@ package com.ays.auth.service.impl;
 
 import com.ays.AbstractUnitTest;
 import com.ays.auth.model.entity.AysInvalidTokenEntity;
+import com.ays.auth.model.entity.AysInvalidTokenEntityBuilder;
 import com.ays.auth.repository.AysInvalidTokenRepository;
+import com.ays.auth.util.exception.TokenAlreadyInvalidatedException;
 import com.ays.common.util.AysRandomUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -40,6 +44,43 @@ class AysInvalidTokenServiceImplTest extends AbstractUnitTest {
 
         Mockito.verify(invalidTokenRepository, Mockito.times(1))
                 .saveAll(mockInvalidTokenEntities);
+    }
+
+    @Test
+    void givenInvalidTokenId_whenTokenIdValidated_thenThrowTokenAlreadyInvalidatedException() {
+        // Given
+        AysInvalidTokenEntity mockInvalidTokenEntity = new AysInvalidTokenEntityBuilder().withValidValues().build();
+        String mockTokenId = mockInvalidTokenEntity.getTokenId();
+
+        // When
+        Mockito.when(invalidTokenRepository.findByTokenId(mockTokenId))
+                .thenReturn(Optional.of(mockInvalidTokenEntity));
+
+        // Then
+        Assertions.assertThrows(
+                TokenAlreadyInvalidatedException.class,
+                () -> invalidTokenService.checkForInvalidityOfToken(mockTokenId)
+        );
+
+
+        Mockito.verify(invalidTokenRepository, Mockito.times(1))
+                .findByTokenId(mockTokenId);
+    }
+
+    @Test
+    void givenValidTokenId_whenTokenIdValid_thenDoNothing() {
+        // Given
+        String mockTokenId = AysRandomUtil.generateUUID();
+
+        // When
+        Mockito.when(invalidTokenRepository.findByTokenId(mockTokenId))
+                .thenReturn(Optional.empty());
+
+        // Then
+        invalidTokenService.checkForInvalidityOfToken(mockTokenId);
+
+        Mockito.verify(invalidTokenRepository, Mockito.times(1))
+                .findByTokenId(mockTokenId);
     }
 
 }
