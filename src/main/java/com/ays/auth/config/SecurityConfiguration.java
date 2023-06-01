746380418b12214a336c2c8f59709576a7d8e126
ilkerCelimli <ilkerCelimli@outlook.com>
2023-06-01 16:37:22 +0300

    deleted unused imports

--- src/main/java/com/ays/auth/config/SecurityConfiguration.java
+++ src/main/java/com/ays/auth/config/SecurityConfiguration.java
@@ -21,7 +21,6 @@ import org.springframework.security.oauth2.server.resource.web.authentication.Be
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
