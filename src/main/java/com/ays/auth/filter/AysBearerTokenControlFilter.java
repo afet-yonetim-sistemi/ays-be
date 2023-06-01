8f05a20ffeedd67fc3c51012102ef1a5e49265a4
ilkerCelimli <ilkerCelimli@outlook.com>
2023-06-01 16:37:07 +0300

    deleted unused imports

--- src/main/java/com/ays/auth/filter/AysBearerTokenControlFilter.java
+++ src/main/java/com/ays/auth/filter/AysBearerTokenControlFilter.java
@@ -6,11 +6,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
