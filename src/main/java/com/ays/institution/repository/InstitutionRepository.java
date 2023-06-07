10146abb6b938c069274fa228b321dc9b5f75e1f
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-06-07 20:56:27 +0300

    Java DOCs Has Been Fixed for `InstitutionRepository` Class

--- src/main/java/com/ays/institution/repository/InstitutionRepository.java
+++ src/main/java/com/ays/institution/repository/InstitutionRepository.java
@@ -4,8 +4,8 @@ import com.ays.institution.model.entity.InstitutionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * An interface for accessing and managing organizations in a data source with CRUD operations.
- * It extends the JpaRepository interface with OrganizationEntity as the entity type and String as the ID type.
+ * An interface for accessing and managing institutions in a data source with CRUD operations.
+ * It extends the JpaRepository interface with InstitutionEntity as the entity type and String as the ID type.
  * The default behavior of the repository can be extended by adding custom methods to this interface.
  */
 public interface InstitutionRepository extends JpaRepository<InstitutionEntity, String> {
