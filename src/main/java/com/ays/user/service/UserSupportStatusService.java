fdfed25d8495613fe19c9f62f6efa118ec296ce7
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-06-11 22:56:38 +0300

    Add Java Doc for UserSupportStatusService

--- src/main/java/com/ays/user/service/UserSupportStatusService.java
+++ src/main/java/com/ays/user/service/UserSupportStatusService.java
@@ -2,7 +2,15 @@ package com.ays.user.service;
 
 import com.ays.user.model.dto.request.UserSupportStatusUpdateRequest;
 
+/**
+ * User Support Status Service interface for managing the support status of a user.
+ */
 public interface UserSupportStatusService {
 
+    /**
+     * Updates the support status of a user.
+     *
+     * @param updateRequest the request object containing the updated support status
+     */
     void updateUserSupportStatus(UserSupportStatusUpdateRequest updateRequest);
 }
