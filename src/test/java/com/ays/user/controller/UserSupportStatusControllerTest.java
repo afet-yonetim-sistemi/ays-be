7b41e8bca66a51e07eaef12c4736cbdb091c53cd
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-06-12 20:56:45 +0300

    Define UserSupportStatusControllerTest

---
+++ src/test/java/com/ays/user/controller/UserSupportStatusControllerTest.java
@@ -0,0 +1,88 @@
+package com.ays.user.controller;
+
+import com.ays.AbstractRestControllerTest;
+import com.ays.common.model.dto.response.AysResponse;
+import com.ays.common.model.dto.response.AysResponseBuilder;
+import com.ays.common.util.exception.model.AysError;
+import com.ays.user.model.dto.request.UserSupportStatusUpdateRequest;
+import com.ays.user.model.dto.request.UserSupportStatusUpdateRequestBuilder;
+import com.ays.user.model.enums.UserSupportStatus;
+import com.ays.user.service.UserSupportStatusService;
+import com.ays.util.AysMockMvcRequestBuilders;
+import com.ays.util.AysMockResultMatchersBuilders;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+
+class UserSupportStatusControllerTest extends AbstractRestControllerTest {
+
+    @MockBean
+    private UserSupportStatusService supportStatusService;
+
+
+    private static final String BASE_PATH = "/api/v1/user/self/status/support";
+
+    @Test
+    void givenValidUserSupportStatusUpdateRequest_whenUserRole_thenReturnSuccess() throws Exception {
+
+        // Given
+        UserSupportStatus userSupportStatus = UserSupportStatus.READY;
+
+        UserSupportStatusUpdateRequest mockUserSupportStatusUpdateRequest =
+                new UserSupportStatusUpdateRequestBuilder().withSupportStatus(userSupportStatus).build();
+
+        // When
+        Mockito.doNothing().when(supportStatusService).updateUserSupportStatus(mockUserSupportStatusUpdateRequest);
+
+        // Then
+        AysResponse<Void> mockAysResponse = AysResponse.SUCCESS;
+
+        mockMvc.perform(AysMockMvcRequestBuilders
+                        .put(BASE_PATH, mockUserToken.getAccessToken(), mockUserSupportStatusUpdateRequest))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(AysMockResultMatchersBuilders.status().isOk())
+                .andExpect(AysMockResultMatchersBuilders.time()
+                        .isNotEmpty())
+                .andExpect(AysMockResultMatchersBuilders.httpStatus()
+                        .value(mockAysResponse.getHttpStatus().getReasonPhrase()))
+                .andExpect(AysMockResultMatchersBuilders.isSuccess()
+                        .value(mockAysResponse.getIsSuccess()))
+                .andExpect(AysMockResultMatchersBuilders.response()
+                        .doesNotExist());
+
+        Mockito.verify(supportStatusService, Mockito.times(1))
+                .updateUserSupportStatus(mockUserSupportStatusUpdateRequest);
+
+    }
+
+    @Test
+    void givenValidUserSupportStatusUpdateRequest_whenAdminRole_thenReturnAccessDeniedException() throws Exception {
+        // Given
+        UserSupportStatus userSupportStatus = UserSupportStatus.READY;
+
+        UserSupportStatusUpdateRequest mockUserSupportStatusUpdateRequest =
+                new UserSupportStatusUpdateRequestBuilder().withSupportStatus(userSupportStatus).build();
+
+        // Then
+        MockHttpServletRequestBuilder mockHttpServletRequestBuilder = AysMockMvcRequestBuilders
+                .put(BASE_PATH, mockAdminUserToken.getAccessToken(), mockUserSupportStatusUpdateRequest);
+
+        AysResponse<AysError> mockResponse = AysResponseBuilder.FORBIDDEN;
+        mockMvc.perform(mockHttpServletRequestBuilder)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(AysMockResultMatchersBuilders.status().isForbidden())
+                .andExpect(AysMockResultMatchersBuilders.time()
+                        .isNotEmpty())
+                .andExpect(AysMockResultMatchersBuilders.httpStatus()
+                        .value(mockResponse.getHttpStatus().name()))
+                .andExpect(AysMockResultMatchersBuilders.isSuccess()
+                        .value(mockResponse.getIsSuccess()))
+                .andExpect(AysMockResultMatchersBuilders.response()
+                        .doesNotExist());
+
+    }
+
+}
