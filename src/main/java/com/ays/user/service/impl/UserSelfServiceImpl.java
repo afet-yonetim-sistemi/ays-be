c6bcf6bfc10844ebd5c1812f53b524bc6ee56ee0
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-06-16 21:33:47 +0300

    Revise UserSelfServiceImpl

--- src/main/java/com/ays/user/service/impl/UserSelfServiceImpl.java
+++ src/main/java/com/ays/user/service/impl/UserSelfServiceImpl.java
@@ -4,14 +4,14 @@ import com.ays.auth.model.AysIdentity;
 import com.ays.user.model.dto.request.UserSupportStatusUpdateRequest;
 import com.ays.user.model.entity.UserEntity;
 import com.ays.user.repository.UserRepository;
-import com.ays.user.service.UserSupportStatusService;
+import com.ays.user.service.UserSelfService;
 import com.ays.user.util.exception.AysUserNotExistByIdException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserSupportStatusServiceImpl implements UserSupportStatusService {
+public class UserSelfServiceImpl implements UserSelfService {
 
     private final UserRepository userRepository;
 
