d3dcb1f8f71d6fcf87410ee4aa45a60a2635f59a
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-06-18 16:13:01 +0300

    Java DOCs Has Been Added for `AysSpecification` Class

--- src/main/java/com/ays/common/model/AysSpecification.java
+++ src/main/java/com/ays/common/model/AysSpecification.java
@@ -16,9 +16,19 @@ import java.util.Map;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AysSpecification {
 
+    /**
+     * A builder class for creating specifications in the context of AysSpecification.
+     * Use this builder to construct predicates based on a provided filter.
+     */
     @SuppressWarnings("unused")
     public static class AysSpecificationBuilder<C> {
 
+        /**
+         * Constructs a Specification by applying the provided filter as an AND condition.
+         *
+         * @param filter a map representing the filter conditions, where the key is the field name and the value is the desired value
+         * @return a Specification representing the AND condition of the provided filter
+         */
         public Specification<C> and(final Map<String, Object> filter) {
 
             List<Predicate> predicates = new ArrayList<>();
