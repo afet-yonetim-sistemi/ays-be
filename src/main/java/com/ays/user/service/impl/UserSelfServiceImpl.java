f28ece69326988ebd45f3382a05d5b62034854b4
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-06-18 19:59:51 +0300

    Revise UserSelfServiceImpl

--- src/main/java/com/ays/user/service/impl/UserSelfServiceImpl.java
+++ src/main/java/com/ays/user/service/impl/UserSelfServiceImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @RequiredArgsConstructor
-public class UserSelfServiceImpl implements UserSelfService {
+class UserSelfServiceImpl implements UserSelfService {
 
     private final UserRepository userRepository;
 
