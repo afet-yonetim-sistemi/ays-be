fedc158ac23af5167f532cb45ccb3838be843f9a
skayikci <serhat.kayikci@gmail.com>
2023-06-28 22:02:58 +0200

    add build step

---
+++ build/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo 'deploy is running'
+
+# connect to the server
+echo 'connecting to server'
+echo "$PRIVATE_KEY" > private_key && chmod 600 private_key
+ssh -o StrictHostKeyChecking=no -i private_key "${USER_NAME}"@"${HOSTNAME}"
+
+# pull the latest changes
+echo 'pulling latest changes from github'
+cd "${PATH}" || exit
+git status
+git checkout main &&
+git fetch --all &&
+git reset --hard origin/main &&
+git pull origin main
+
+# stop already existing images
+echo 'stopping already existing images'
+docker ps
+docker stop "$(docker ps -aq)"
+docker rm "$(docker ps -aq)"
+docker rmi "$(docker images -q)"
+
+# run the docker file
+echo 'run new docker images'
+docker compose up -f ../docker-compose-production.yml -d --build
