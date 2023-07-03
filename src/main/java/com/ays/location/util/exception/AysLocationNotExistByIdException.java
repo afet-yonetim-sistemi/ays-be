b7f412dc3a382038192271267be97c0960c55735
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-07-03 20:54:16 +0300

    Add Exception Handling for Location

---
+++ src/main/java/com/ays/location/util/exception/AysLocationNotExistByIdException.java
@@ -0,0 +1,27 @@
+package com.ays.location.util.exception;
+
+import com.ays.common.util.exception.AysNotExistException;
+
+import java.io.Serial;
+
+/**
+ * Exception to be thrown when a location with a given ID does not exist.
+ */
+public class AysLocationNotExistByIdException extends AysNotExistException {
+
+    /**
+     * Unique serial version ID.
+     */
+    @Serial
+    private static final long serialVersionUID = 2448425046125508633L;
+
+    /**
+     * Constructs a new AysLocationNotExistByIdException with the specified assignment ID.
+     *
+     * @param id the ID of the location that does not exist
+     */
+    public AysLocationNotExistByIdException(String id) {
+        super("LOCATION NOT EXIST! id:" + id);
+    }
+
+}
