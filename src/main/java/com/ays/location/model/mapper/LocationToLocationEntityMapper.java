c3f42848d0bd9e4909e568bfa937b347bc759c8f
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-07-03 20:53:44 +0300

    Add Mapper process for Location

---
+++ src/main/java/com/ays/location/model/mapper/LocationToLocationEntityMapper.java
@@ -0,0 +1,28 @@
+package com.ays.location.model.mapper;
+
+import com.ays.common.model.mapper.BaseMapper;
+import com.ays.location.model.Location;
+import com.ays.location.model.entity.LocationEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * LocationToLocationEntityMapper is an interface that defines the mapping between an {@link Location} and an {@link LocationEntity}.
+ * This interface uses the MapStruct annotation @Mapper to generate an implementation of this interface at compile-time.
+ * <p>The class provides a static method {@code initialize()} that returns an instance of the generated mapper implementation.
+ * <p>The interface extends the MapStruct interface {@link BaseMapper}, which defines basic mapping methods.
+ * The interface adds no additional mapping methods, but simply defines the types to be used in the mapping process.
+ */
+@Mapper
+public interface LocationToLocationEntityMapper extends BaseMapper<Location, LocationEntity> {
+
+    /**
+     * Initializes the mapper.
+     *
+     * @return the initialized mapper object.
+     */
+    static LocationToLocationEntityMapper initialize() {
+        return Mappers.getMapper(LocationToLocationEntityMapper.class);
+    }
+
+}
