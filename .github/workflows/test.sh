b3284ab3d142c67579bbe4ac61361eeecd4c7239
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-07-15 14:03:16 +0300

    Deployment Test for TEST Environment

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -4,13 +4,13 @@ echo 'deployment is starting...'
 
 echo 'connecting to server'
 echo "${AWS_PRIVATE_KEY}" >private_key && chmod 600 private_key
-ssh -o StrictHostKeyChecking=no -i private_key "${AWS_USERNAME}"@"${AWS_TEST_ENVIRONMENT_IP}" || exit
+ssh -o StrictHostKeyChecking=no -i private_key "${AWS_USERNAME}"@"${AWS_TEST_ENVIRONMENT_IP}" ; echo 'connected' || exit ; echo 'not connected!'
 
 echo 'pulling latest changes from github and rebuild docker image'
 ssh -o StrictHostKeyChecking=no -i private_key "${AWS_USERNAME}"@"${AWS_TEST_ENVIRONMENT_IP}" "
   cd ${AWS_BE_APP_PATH} ;
   sudo  git status ;
-  sudo  git switch hotfix/build-flow ;
+  sudo  git switch main ;
   sudo  git fetch --all ;
   sudo  git pull ;
   sudo  docker compose -f docker-compose-test.yml up -d --build
