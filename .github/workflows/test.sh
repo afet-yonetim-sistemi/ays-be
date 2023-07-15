d9bcc2887f7a8a4e3e9b3358f77963bb6dff4e39
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-07-15 13:56:31 +0300

    Deployment Test for TEST Environment

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -4,8 +4,8 @@ echo 'deploy is running'
 
 # connect to the server
 echo 'connecting to server'
-echo "${AWS_PRIVATE_KEY}" > private_key && chmod 600 private_key
-ssh -o StrictHostKeyChecking=no -i private_key ${AWS_USERNAME}@${AWS_TEST_ENVIRONMENT_IP} "
+echo "${AWS_PRIVATE_KEY}" >private_key && chmod 600 private_key
+ssh -o StrictHostKeyChecking=no -i private_key "${AWS_USERNAME}"@"${AWS_TEST_ENVIRONMENT_IP}" "
   cd ${AWS_BE_APP_PATH} ;
   sudo  git status ;
   sudo  git switch hotfix/build-flow ;
