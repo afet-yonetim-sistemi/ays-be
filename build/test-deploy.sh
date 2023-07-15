e40f95e2df431a2d392a8a9997889e2b7799185d
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-07-15 12:31:47 +0300

    `sudo su` Command Has Been Added to `test-deploy.sh` and `live-deploy.sh` Files

--- build/test-deploy.sh
+++ build/test-deploy.sh
@@ -6,6 +6,7 @@ echo 'deploy is running'
 echo 'connecting to server'
 echo "${AWS_PRIVATE_KEY}" > private_key && chmod 600 private_key
 ssh -o StrictHostKeyChecking=no -i private_key ${AWS_USERNAME}@${AWS_TEST_ENVIRONMENT_IP}
+sudo su
 
 # pull the latest changes
 echo 'pulling latest changes from github'
