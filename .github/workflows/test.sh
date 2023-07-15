8fc552486a480144d75b35f0673cf0efb4c37f0e
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-07-15 13:13:56 +0300

    Deployment Test for TEST Environment

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -5,7 +5,7 @@ echo 'deploy is running'
 # connect to the server
 echo 'connecting to server and'
 echo "${AWS_PRIVATE_KEY}" > private_key && chmod 600 private_key
-ssh -o StrictHostKeyChecking=no -i private_key ${AWS_USERNAME}@${AWS_TEST_ENVIRONMENT_IP} ; sudo su ; pwd ; cd ${AWS_BE_APP_PATH} ; pwd ; git status ; git switch main ; git fetch --all ; git pull origin main || exit
+ssh -o StrictHostKeyChecking=no -i private_key ${AWS_USERNAME}@${AWS_TEST_ENVIRONMENT_IP} && sudo su && pwd ; cd ${AWS_BE_APP_PATH} ; pwd ; git status ; git switch main ; git fetch --all ; git pull origin main || exit
 
 # run the docker file
 echo 'run new docker images'
