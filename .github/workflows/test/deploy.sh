8ce564689b261a605aebb9d7517870b612ae2e73
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-07-16 16:31:56 +0300

    `deploy.sh` and `deployment.yml` Files Have Been Fixed for TEST and LIVE Environments

--- .github/workflows/test/deploy.sh
+++ .github/workflows/test/deploy.sh
@@ -32,10 +32,10 @@ echo 'pulling latest changes from github and rebuild docker image'
 ssh -o StrictHostKeyChecking=no -i private_key "${AWS_USERNAME}"@"${AWS_TEST_ENVIRONMENT_IP}" "
   cd ${AWS_BE_APP_PATH};
   sudo git status;
-  sudo git switch ${GH_TEST_DEPLOYMENT_BRANCH};
-  sudo git reset --hard;
   sudo git fetch --all;
+  sudo git switch ${GH_TEST_DEPLOYMENT_BRANCH};
   sudo git pull;
+  sudo git reset --hard;
   sudo docker compose -f docker-compose-test.yml up -d --build
 " || exit
 
