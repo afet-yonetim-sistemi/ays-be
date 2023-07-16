4c34c1d961c97b315a8bacf74950860c3b4fc908
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-07-16 21:45:20 +0300

    Revise UserLocationEntityToUserLocationMapper and UserLocationToUserLocationEntityMapper

--- src/main/java/com/ays/location/model/mapper/UserLocationToUserLocationEntityMapper.java
+++ src/main/java/com/ays/location/model/mapper/UserLocationToUserLocationEntityMapper.java
@@ -1,28 +1,28 @@
 package com.ays.location.model.mapper;
 
 import com.ays.common.model.mapper.BaseMapper;
-import com.ays.location.model.Location;
+import com.ays.location.model.UserLocation;
 import com.ays.location.model.entity.UserLocationEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 /**
- * LocationEntityToLocationMapper is an interface that defines the mapping between an {@link UserLocationEntity} and an {@link Location}.
+ * LocationToLocationEntityMapper is an interface that defines the mapping between an {@link UserLocation} and an {@link UserLocationEntity}.
  * This interface uses the MapStruct annotation @Mapper to generate an implementation of this interface at compile-time.
  * <p>The class provides a static method {@code initialize()} that returns an instance of the generated mapper implementation.
  * <p>The interface extends the MapStruct interface {@link BaseMapper}, which defines basic mapping methods.
  * The interface adds no additional mapping methods, but simply defines the types to be used in the mapping process.
  */
 @Mapper
-public interface LocationEntityToLocationMapper extends BaseMapper<UserLocationEntity, Location> {
+public interface UserLocationToUserLocationEntityMapper extends BaseMapper<UserLocation, UserLocationEntity> {
 
     /**
      * Initializes the mapper.
      *
      * @return the initialized mapper object.
      */
-    static LocationEntityToLocationMapper initialize() {
-        return Mappers.getMapper(LocationEntityToLocationMapper.class);
+    static UserLocationToUserLocationEntityMapper initialize() {
+        return Mappers.getMapper(UserLocationToUserLocationEntityMapper.class);
     }
 
 }
