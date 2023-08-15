9be64712d9d11d5957e7083a8900dd75e4f7ad49
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-08-15 21:41:48 +0300

    Implement Builder classes for Assignment

---
+++ src/test/java/com/ays/assignment/model/AssignmentBuilder.java
@@ -0,0 +1,11 @@
+package com.ays.assignment.model;
+
+import com.ays.common.model.TestDataBuilder;
+
+public class AssignmentBuilder extends TestDataBuilder<Assignment> {
+
+    public AssignmentBuilder() {
+        super(Assignment.class);
+    }
+
+}
