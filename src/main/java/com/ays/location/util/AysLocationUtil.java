544a6c4dfd3692be0c5cfa01473eea6d6fe98d62
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-08-19 18:30:51 +0300

    `AysLocationUtil` Class Has Been Created for Logical Methods of Location

---
+++ src/main/java/com/ays/location/util/AysLocationUtil.java
@@ -0,0 +1,28 @@
+package com.ays.location.util;
+
+import lombok.experimental.UtilityClass;
+import org.locationtech.jts.geom.*;
+import org.locationtech.jts.geom.impl.CoordinateArraySequence;
+
+/**
+ * Utility class for handling location-related operations.
+ * Provides methods to generate geometric points based on latitude and longitude coordinates.
+ */
+@UtilityClass
+public class AysLocationUtil {
+
+    /**
+     * Generates a Point object representing a location based on the provided latitude and longitude coordinates.
+     *
+     * @param latitude  The latitude coordinate of the location.
+     * @param longitude The longitude coordinate of the location.
+     * @return A Point object representing the location.
+     */
+    public static Point generatePoint(final Double latitude, final Double longitude) {
+        final Coordinate[] coordinates = new Coordinate[]{new Coordinate(latitude, longitude)};
+        final CoordinateSequence coordinateSequence = new CoordinateArraySequence(coordinates);
+        final PrecisionModel precisionModel = new PrecisionModel(PrecisionModel.FLOATING);
+        return new GeometryFactory(precisionModel, 4326).createPoint(coordinateSequence);
+    }
+
+}
