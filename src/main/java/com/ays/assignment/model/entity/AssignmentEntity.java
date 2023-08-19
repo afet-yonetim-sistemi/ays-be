47392dd82810b074629dac0d8670bf8aa4eeacf6
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-08-19 14:33:47 +0300

    Revise AssignmentEntity

--- src/main/java/com/ays/assignment/model/entity/AssignmentEntity.java
+++ src/main/java/com/ays/assignment/model/entity/AssignmentEntity.java
@@ -95,15 +95,9 @@ public class AssignmentEntity extends BaseEntity {
         return AssignmentStatus.DONE.equals(this.status);
     }
 
-    public boolean isDeleted() {
-        return AssignmentStatus.DELETED.equals(this.status);
-    }
 
     public void updateAssignmentStatus(AssignmentStatus assignmentStatus) {
         this.status = assignmentStatus;
     }
 
-    public void deleteUser() {
-        this.status = AssignmentStatus.DELETED;
-    }
 }
