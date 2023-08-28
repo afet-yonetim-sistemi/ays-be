f9cbacfc677a977c06164f77814b595e49b8c9cd
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-08-29 02:05:05 +0300

    Define getAssignments in AssignmentService

--- src/main/java/com/ays/assignment/service/AssignmentService.java
+++ src/main/java/com/ays/assignment/service/AssignmentService.java
@@ -2,6 +2,8 @@ package com.ays.assignment.service;
 
 
 import com.ays.assignment.model.Assignment;
+import com.ays.assignment.model.dto.request.AssignmentListRequest;
+import com.ays.common.model.AysPage;
 
 /**
  * Assignment Save Service to perform assignment related business operations.
@@ -15,4 +17,12 @@ public interface AssignmentService {
      * @return Assignment
      */
     Assignment getAssignmentById(String id);
+
+    /**
+     * Get Assignments based on the specified filters in the {@link AssignmentListRequest}
+     *
+     * @param listRequest The request dto object
+     * @return Assignments list
+     */
+    AysPage<Assignment> getAssignments(AssignmentListRequest listRequest);
 }
