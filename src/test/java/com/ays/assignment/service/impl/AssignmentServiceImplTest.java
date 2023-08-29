88aebbc02f09f6806720f809b86def70581a1c19
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-08-29 23:44:23 +0300

    Handled givenAssignmentListRequest_whenAssignmentStatusIsAvailable_thenReturnAysPageAssignmentResponse

--- src/test/java/com/ays/assignment/service/impl/AssignmentServiceImplTest.java
+++ src/test/java/com/ays/assignment/service/impl/AssignmentServiceImplTest.java
@@ -2,19 +2,29 @@ package com.ays.assignment.service.impl;
 
 import com.ays.AbstractUnitTest;
 import com.ays.assignment.model.Assignment;
+import com.ays.assignment.model.dto.request.AssignmentListRequest;
+import com.ays.assignment.model.dto.request.AssignmentListRequestBuilder;
 import com.ays.assignment.model.entity.AssignmentEntity;
 import com.ays.assignment.model.entity.AssignmentEntityBuilder;
 import com.ays.assignment.model.mapper.AssignmentEntityToAssignmentMapper;
 import com.ays.assignment.repository.AssignmentRepository;
 import com.ays.assignment.util.exception.AysAssignmentNotExistByIdException;
 import com.ays.auth.model.AysIdentity;
+import com.ays.common.model.AysPage;
+import com.ays.common.model.AysPageBuilder;
 import com.ays.common.util.AysRandomUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -92,5 +102,34 @@ class AssignmentServiceImplTest extends AbstractUnitTest {
                 .thenReturn(Optional.empty());
         Mockito.verify(identity, Mockito.times(1))
                 .getInstitutionId();
+    }
+
+    @Test
+    void givenAssignmentListRequest_whenAssignmentStatusIsAvailable_thenReturnAysPageAssignmentResponse() {
+
+        // Given
+        String mockInstitutionId = AysRandomUtil.generateUUID();
+
+        AssignmentListRequest assignmentListRequest = new AssignmentListRequestBuilder().withValidValuesForStatus().build();
+
+        List<AssignmentEntity> mockAssignmentEntities = Collections.singletonList(new AssignmentEntityBuilder()
+                .withValidFields().build());
+        Page<AssignmentEntity> mockPageAssignmentEntities = new PageImpl<>(mockAssignmentEntities);
+
+        List<Assignment> mockAssignments = ASSIGNMENT_ENTITY_TO_ASSIGNMENT_MAPPER.map(mockAssignmentEntities);
+        AysPage<Assignment> mockAysPageAssignments = AysPage.of(assignmentListRequest.getFilter(), mockPageAssignmentEntities, mockAssignments);
+
+        // When
+        Mockito.when(identity.getInstitutionId()).thenReturn(mockInstitutionId);
+        Mockito.when(assignmentRepository.findAll(Mockito.any(Specification.class), Mockito.any(Pageable.class)))
+                .thenReturn(mockPageAssignmentEntities);
+
+        // Then
+        AysPage<Assignment> aysPageAssignment = assignmentService.getAssignments(assignmentListRequest);
+
+        AysPageBuilder.assertEquals(mockAysPageAssignments, aysPageAssignment);
+
+        Mockito.verify(assignmentRepository, Mockito.times(1))
+                .findAll(Mockito.any(Specification.class), Mockito.any(Pageable.class));
     }
 }
