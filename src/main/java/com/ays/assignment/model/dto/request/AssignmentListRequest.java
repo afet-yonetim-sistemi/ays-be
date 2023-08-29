21b18360ee60b7500f7b61bf608203012e87b56c
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-08-29 15:21:37 +0300

    Add phoneNumber filter to AssignmentListRequest

--- src/main/java/com/ays/assignment/model/dto/request/AssignmentListRequest.java
+++ src/main/java/com/ays/assignment/model/dto/request/AssignmentListRequest.java
@@ -2,12 +2,12 @@ package com.ays.assignment.model.dto.request;
 
 import com.ays.assignment.model.enums.AssignmentStatus;
 import com.ays.common.model.AysFiltering;
+import com.ays.common.model.AysPhoneNumber;
 import com.ays.common.model.dto.request.AysFilteringRequest;
 import com.ays.common.model.dto.request.AysPagingRequest;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -40,9 +40,9 @@ public class AssignmentListRequest extends AysPagingRequest implements AysFilter
         /**
          * List of assignment statuses used for filtering.
          */
-        @NotNull
-        public List<AssignmentStatus> statuses;
+        private List<AssignmentStatus> statuses;
 
+        private AysPhoneNumber phoneNumber;
     }
 
 
@@ -62,22 +62,48 @@ public class AssignmentListRequest extends AysPagingRequest implements AysFilter
 
 
     /**
-     * Converts the request into a JPA Specification that filters assignments based on the specified statuses,
-     * if they are provided.
+     * Converts the request into a JPA Specification that filters assignments based on the specified
+     * statuses and phoneNumber, if they are provided.
      *
      * @param clazz the class type of the specification.
      * @return the generated JPA Specification based on the request filters.
      */
     @Override
     public <C> Specification<C> toSpecification(Class<C> clazz) {
+
         if (this.filter == null) {
             return Specification.allOf();
         }
 
-        return this.filter.statuses.stream().map(status ->
-                (Specification<C>) (root, query, criteriaBuilder) ->
-                        criteriaBuilder.equal(root.get("status"), status))
-                .reduce(Specification::or).orElse(null);
+        Specification<C> specification = Specification.where(null);
+
+        if (this.filter.phoneNumber != null) {
+
+            if (this.filter.phoneNumber.getLineNumber() != null) {
+                Specification<C> lineNumberSpecification = (root, query, criteriaBuilder) ->
+                        criteriaBuilder.equal(root.get("lineNumber"), this.filter.phoneNumber.getLineNumber());
+
+                specification = specification.and(lineNumberSpecification);
+            }
+
+            if (this.filter.phoneNumber.getCountryCode() != null) {
+                Specification<C> countryCodeSpecification = (root, query, criteriaBuilder)
+                        -> criteriaBuilder.equal(root.get("countryCode"), this.filter.phoneNumber.getCountryCode());
+
+                specification = specification.and(countryCodeSpecification);
+            }
+        }
+
+        if (this.filter.statuses != null) {
+            Specification<C> statusSpecification = this.filter.statuses.stream().map(status ->
+                            (Specification<C>) (root, query, criteriaBuilder) ->
+                                    criteriaBuilder.equal(root.get("status"), status))
+                    .reduce(Specification::or).orElse(null);
+
+            specification = specification.and(statusSpecification);
+        }
+
+        return specification;
     }
 
 }
