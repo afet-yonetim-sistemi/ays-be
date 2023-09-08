d07d6f4269274f37dcfcf390cdf9c4fd9a33ec78
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-09 01:36:44 +0300

    Add updateAssignment method to AssignmentService

--- src/main/java/com/ays/assignment/service/AssignmentService.java
+++ src/main/java/com/ays/assignment/service/AssignmentService.java
@@ -2,6 +2,7 @@ package com.ays.assignment.service;
 
 
 import com.ays.assignment.model.Assignment;
+import com.ays.assignment.model.dto.request.AssignmentUpdateRequest;
 
 /**
  * Assignment Save Service to perform assignment related business operations.
@@ -15,4 +16,7 @@ public interface AssignmentService {
      * @return Assignment
      */
     Assignment getAssignmentById(String id);
+
+    void updateAssignment(final String id, AssignmentUpdateRequest updateRequest);
+
 }
