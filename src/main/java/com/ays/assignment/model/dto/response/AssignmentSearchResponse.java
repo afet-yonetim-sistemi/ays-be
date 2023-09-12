e002549cf9ca671df06665d6421d351a53118653
İbrahim Deniz <dnzibrahim1@gmail.com>
2023-09-12 20:56:09 +0300

    #163 | Fixed response type of search assignment endpoint (#167)

--- src/main/java/com/ays/assignment/model/dto/response/AssignmentSearchResponse.java
+++ src/main/java/com/ays/assignment/model/dto/response/AssignmentSearchResponse.java
@@ -2,9 +2,11 @@ package com.ays.assignment.model.dto.response;
 
 import com.ays.assignment.model.enums.AssignmentStatus;
 import com.ays.common.model.dto.response.BaseResponse;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+import org.locationtech.jts.geom.Point;
 
 /**
  * This class represents the response for a user with assignment.
@@ -17,8 +19,21 @@ public class AssignmentSearchResponse extends BaseResponse {
 
     private String id;
     private String description;
-    private Double longitude;
-    private Double latitude;
+    private Location location;
     private AssignmentStatus status;
+
+    @Getter
+    @Builder
+    public static class Location {
+        private Double longitude;
+        private Double latitude;
+    }
+
+    public void setLocation(Point point) {
+        this.location = Location.builder()
+                .longitude(point.getX())
+                .latitude(point.getY())
+                .build();
+    }
 
 }
