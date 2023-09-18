b65bac0376febaa85431db52bdd0e83babb3f843
ibrahimdenizz <dnzibrahim1@gmail.com>
2023-09-18 18:29:44 +0300

    Fix java doc in AysUserAlreadyHasAssignmentException

--- src/main/java/com/ays/user/util/exception/AysUserAlreadyHasAssignmentException.java
+++ src/main/java/com/ays/user/util/exception/AysUserAlreadyHasAssignmentException.java
@@ -5,7 +5,7 @@ import com.ays.common.util.exception.AysAlreadyException;
 import java.io.Serial;
 
 /**
- * Exception thrown when a user is passive and attempting to perform an action that requires a passive user.
+ * Exception thrown when a user has assignment and attempting to update user support status.
  */
 public class AysUserAlreadyHasAssignmentException extends AysAlreadyException {
 
@@ -16,9 +16,10 @@ public class AysUserAlreadyHasAssignmentException extends AysAlreadyException {
     private static final long serialVersionUID = -3686691276790127586L;
 
     /**
-     * Constructs a new {@code AysUserAlreadyPassiveException} with the specified id.
+     * Constructs a new {@code AysUserAlreadyHasAssignmentException} with the specified id and assignmentId.
      *
-     * @param id the id of the passive user
+     * @param id the id of the user
+     * @param assignmentId the id of the assignment
      */
     public AysUserAlreadyHasAssignmentException(String id, String assignmentId) {
         super("USER ALREADY HAS ASSIGNMENT! id:" + id + " assignmentId:" + assignmentId);
