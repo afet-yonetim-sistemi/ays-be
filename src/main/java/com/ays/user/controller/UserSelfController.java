6ca8eee10813bf4c9714baf28e4bf14f911a694f
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-11-05 15:15:50 +0300

    Imports of `UserSelfController` Class Has Been Refactored

--- src/main/java/com/ays/user/controller/UserSelfController.java
+++ src/main/java/com/ays/user/controller/UserSelfController.java
@@ -9,7 +9,11 @@ import com.ays.user.service.UserSelfService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * User Self Controller class for managing user's own operations.
