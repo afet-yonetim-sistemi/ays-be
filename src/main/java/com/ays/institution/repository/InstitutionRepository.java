fbaa7b4cb4d2adcc8da302d28375a10b73f7016c
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-11-08 00:48:43 +0300

    Add findAllByStatus method to InstitutionRepository

--- src/main/java/com/ays/institution/repository/InstitutionRepository.java
+++ src/main/java/com/ays/institution/repository/InstitutionRepository.java
@@ -1,6 +1,9 @@
 package com.ays.institution.repository;
 
 import com.ays.institution.model.entity.InstitutionEntity;
+import com.ays.institution.model.enums.InstitutionStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -9,4 +12,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * The default behavior of the repository can be extended by adding custom methods to this interface.
  */
 public interface InstitutionRepository extends JpaRepository<InstitutionEntity, String> {
+
+    /**
+     * Find all institutions by status
+     *
+     * @param status   the status of the institutions to retrieve
+     * @param pageable the pagination information
+     * @return the page of institutions
+     */
+    Page<InstitutionEntity> findAllByStatus(InstitutionStatus status, Pageable pageable);
+
 }
