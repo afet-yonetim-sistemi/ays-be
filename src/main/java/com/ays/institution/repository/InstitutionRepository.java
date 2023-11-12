dfb37e513896dde14cbf93d6e1ba5b83d5c6b940
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-11-12 18:05:18 +0300

    Define InstitutionRepository

--- src/main/java/com/ays/institution/repository/InstitutionRepository.java
+++ src/main/java/com/ays/institution/repository/InstitutionRepository.java
@@ -2,9 +2,9 @@ package com.ays.institution.repository;
 
 import com.ays.institution.model.entity.InstitutionEntity;
 import com.ays.institution.model.enums.InstitutionStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * An interface for accessing and managing institutions in a data source with CRUD operations.
@@ -16,10 +16,9 @@ public interface InstitutionRepository extends JpaRepository<InstitutionEntity, 
     /**
      * Find all institutions by status
      *
-     * @param status   the status of the institutions to retrieve
-     * @param pageable the pagination information
-     * @return the page of institutions
+     * @param status the status of the institutions to retrieve
+     * @return the list of institutions
      */
-    Page<InstitutionEntity> findAllByStatus(InstitutionStatus status, Pageable pageable);
+    List<InstitutionEntity> findAllByStatusOrderByNameAsc(InstitutionStatus status);
 
 }
