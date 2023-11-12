07da067874f8b681e904d37acb8982863af27ed0
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-11-12 19:27:03 +0300

    Implement getInstitutionsSummary operation of InstitutionServiceImpl

--- src/main/java/com/ays/institution/service/impl/InstitutionServiceImpl.java
+++ src/main/java/com/ays/institution/service/impl/InstitutionServiceImpl.java
@@ -1,20 +1,36 @@
 package com.ays.institution.service.impl;
 
-import com.ays.common.model.AysPage;
-import com.ays.institution.model.dto.response.InstitutionResponse;
+import com.ays.institution.model.Institution;
+import com.ays.institution.model.entity.InstitutionEntity;
+import com.ays.institution.model.enums.InstitutionStatus;
+import com.ays.institution.model.mapper.InstitutionEntityToInstitutionMapper;
 import com.ays.institution.repository.InstitutionRepository;
 import com.ays.institution.service.InstitutionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+/**
+ * Implementation of the {@link InstitutionService} interface for performing institution-related business operations.
+ */
 @Service
 @RequiredArgsConstructor
 class InstitutionServiceImpl implements InstitutionService {
 
     private final InstitutionRepository institutionRepository;
 
+    private final InstitutionEntityToInstitutionMapper institutionEntityToInstitutionMapper = InstitutionEntityToInstitutionMapper.initialize();
+
+    /**
+     * Retrieves a summary of all active institutions.
+     *
+     * @return a list of {@link Institution} representing the summary of institutions
+     */
     @Override
-    public AysPage<InstitutionResponse> getInstitutionsSummary() {
-        return null;
+    public List<Institution> getInstitutionsSummary() {
+        final List<InstitutionEntity> allByStatusAndOrderByNameAsc = institutionRepository.findAllByStatusOrderByNameAsc(InstitutionStatus.ACTIVE);
+        return institutionEntityToInstitutionMapper.map(allByStatusAndOrderByNameAsc);
     }
+
 }
