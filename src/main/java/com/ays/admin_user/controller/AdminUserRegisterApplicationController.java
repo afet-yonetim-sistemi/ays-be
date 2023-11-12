dc22d0547e2f12b78eb32f863f270a1e8f777063
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-11-13 01:21:04 +0300

    Create the structure for admin register applications

---
+++ src/main/java/com/ays/admin_user/controller/AdminUserRegisterApplicationController.java
@@ -0,0 +1,15 @@
+package com.ays.admin_user.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Admin Register Application controller to perform register application api operations for admins.
+ */
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+public class AdminUserRegisterApplicationController {
+}
