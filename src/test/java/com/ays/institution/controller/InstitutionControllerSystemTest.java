88355d8da5e804bcb787999e6a169c6abbd0083a
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-11-13 21:10:26 +0300

    Implement test cases for summary in InstitutionControllerSystemTest

---
+++ src/test/java/com/ays/institution/controller/InstitutionControllerSystemTest.java
@@ -0,0 +1,95 @@
+package com.ays.institution.controller;
+
+import com.ays.AbstractSystemTest;
+import com.ays.common.model.dto.response.AysResponse;
+import com.ays.common.model.dto.response.AysResponseBuilder;
+import com.ays.common.util.exception.model.AysError;
+import com.ays.institution.model.Institution;
+import com.ays.institution.model.dto.response.InstitutionResponse;
+import com.ays.institution.model.entity.InstitutionBuilder;
+import com.ays.institution.model.mapper.InstitutionToInstitutionResponseMapper;
+import com.ays.institution.service.InstitutionService;
+import com.ays.util.AysMockMvcRequestBuilders;
+import com.ays.util.AysMockResultMatchersBuilders;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.Arrays;
+import java.util.List;
+
+class InstitutionControllerSystemTest extends AbstractSystemTest {
+
+    @MockBean
+    private InstitutionService institutionService;
+
+
+    private final InstitutionToInstitutionResponseMapper institutionToInstitutionResponseMapper = InstitutionToInstitutionResponseMapper.initialize();
+
+
+    private static final String BASE_PATH = "/api/v1/institutions";
+
+    @Test
+    void givenNothing_whenInstitutionStatusActive_thenReturnListInstitutionResponse() throws Exception {
+
+
+        String mockAccessToken = superAdminToken.getAccessToken();
+
+        Institution institution1 = new InstitutionBuilder().withValidFields().build();
+        Institution institution2 = new InstitutionBuilder().withValidFields().build();
+
+
+        List<Institution> mockInstitutions = Arrays.asList(
+                institution1,
+                institution2
+        );
+
+        // when
+        Mockito.when(institutionService.getInstitutionsSummary()).thenReturn(mockInstitutions);
+
+        // then
+        List<InstitutionResponse> mockInstitutionResponseList = institutionToInstitutionResponseMapper.map(mockInstitutions);
+        AysResponse<List<InstitutionResponse>> mockAysResponse = AysResponse.successOf(mockInstitutionResponseList);
+
+        mockMvc.perform(AysMockMvcRequestBuilders
+                        .get(BASE_PATH.concat("/summary"), mockAccessToken))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(AysMockResultMatchersBuilders.status().isOk())
+                .andExpect(AysMockResultMatchersBuilders.time()
+                        .isNotEmpty())
+                .andExpect(AysMockResultMatchersBuilders.httpStatus()
+                        .value(mockAysResponse.getHttpStatus().getReasonPhrase()))
+                .andExpect(AysMockResultMatchersBuilders.isSuccess()
+                        .value(mockAysResponse.getIsSuccess()))
+                .andExpect(AysMockResultMatchersBuilders.response()
+                        .isNotEmpty());
+
+        // verify
+        Mockito.verify(institutionService, Mockito.times(1)).getInstitutionsSummary();
+
+    }
+
+    @Test
+    void givenNothing_whenUserUnauthorizedForSummary_thenReturnAccessDeniedException() throws Exception {
+
+        // Given
+        String mockAccessToken = userTokenOne.getAccessToken();
+
+
+        MockHttpServletRequestBuilder mockHttpServletRequestBuilder = AysMockMvcRequestBuilders
+                .get(BASE_PATH.concat("/summary"), mockAccessToken);
+
+        AysResponse<AysError> mockResponse = AysResponseBuilder.FORBIDDEN;
+        mockMvc.perform(mockHttpServletRequestBuilder)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(AysMockResultMatchersBuilders.status().isForbidden())
+                .andExpect(AysMockResultMatchersBuilders.time().isNotEmpty())
+                .andExpect(AysMockResultMatchersBuilders.httpStatus().value(mockResponse.getHttpStatus().name()))
+                .andExpect(AysMockResultMatchersBuilders.isSuccess().value(mockResponse.getIsSuccess()))
+                .andExpect(AysMockResultMatchersBuilders.response().doesNotExist());
+
+    }
+
+}
