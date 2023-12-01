92c6694697183a97395d8455544357fce33aac77
İbrahim Deniz <dnzibrahim1@gmail.com>
2023-12-01 11:22:26 +0300

    #237 | Implement the Admin User Registration Application List Endpoint (#260)

--- src/main/java/com/ays/admin_user/service/impl/AdminUserRegisterApplicationServiceImpl.java
+++ src/main/java/com/ays/admin_user/service/impl/AdminUserRegisterApplicationServiceImpl.java
@@ -1,9 +1,18 @@
 package com.ays.admin_user.service.impl;
 
+import com.ays.admin_user.model.AdminUserRegisterApplication;
+import com.ays.admin_user.model.dto.request.AdminUserRegisterApplicationListRequest;
+import com.ays.admin_user.model.entity.AdminUserRegisterApplicationEntity;
+import com.ays.admin_user.model.mapper.AdminUserRegisterApplicationEntityToAdminUserRegisterApplicationMapper;
 import com.ays.admin_user.repository.AdminUserRegisterApplicationRepository;
 import com.ays.admin_user.service.AdminUserRegisterApplicationService;
+import com.ays.common.model.AysPage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * This class implements the {@link AdminUserRegisterApplicationService} interface and provides verification operations for admin users.
@@ -16,4 +25,29 @@ public class AdminUserRegisterApplicationServiceImpl implements AdminUserRegiste
 
     private final AdminUserRegisterApplicationRepository adminUserRegisterApplicationRepository;
 
+
+    private final AdminUserRegisterApplicationEntityToAdminUserRegisterApplicationMapper adminUserRegisterApplicationEntityToAdminUserRegisterApplicationMapper = AdminUserRegisterApplicationEntityToAdminUserRegisterApplicationMapper.initialize();
+
+    /**
+     * Retrieves a paginated list of admin user registration applications based on the specified filters
+     *
+     * @param listRequest covering page, pageSize and filters
+     * @return a paginated list of admin user registration applications
+     */
+    @Override
+    public AysPage<AdminUserRegisterApplication> getRegistrationApplications(AdminUserRegisterApplicationListRequest listRequest) {
+        final Specification<AdminUserRegisterApplicationEntity> filters = listRequest
+                .toSpecification(AdminUserRegisterApplicationEntity.class);
+
+        final Page<AdminUserRegisterApplicationEntity> registerApplicationEntities = adminUserRegisterApplicationRepository
+                .findAll(filters, listRequest.toPageable());
+
+        final List<AdminUserRegisterApplication> registerApplications = adminUserRegisterApplicationEntityToAdminUserRegisterApplicationMapper.map(registerApplicationEntities.getContent());
+
+        return AysPage.of(
+                listRequest.getFilter(),
+                registerApplicationEntities,
+                registerApplications
+        );
+    }
 }
--- src/test/java/com/ays/admin_user/controller/AdminUserRegisterApplicationControllerTest.java
+++ src/test/java/com/ays/admin_user/controller/AdminUserRegisterApplicationControllerTest.java
@@ -1,11 +1,100 @@
 package com.ays.admin_user.controller;
 
 import com.ays.AbstractRestControllerTest;
+import com.ays.admin_user.model.AdminUserRegisterApplication;
+import com.ays.admin_user.model.dto.request.AdminUserRegisterApplicationListRequest;
+import com.ays.admin_user.model.dto.request.AdminUserRegisterApplicationListRequestBuilder;
+import com.ays.admin_user.model.dto.response.AdminUserRegisterApplicationsResponse;
+import com.ays.admin_user.model.entity.AdminUserRegisterApplicationEntity;
+import com.ays.admin_user.model.entity.AdminUserRegisterApplicationEntityBuilder;
+import com.ays.admin_user.model.mapper.AdminUserRegisterApplicationEntityToAdminUserRegisterApplicationMapper;
+import com.ays.admin_user.model.mapper.AdminUserRegisterApplicationToAdminUserRegisterApplicationsResponseMapper;
 import com.ays.admin_user.service.AdminUserRegisterApplicationService;
+import com.ays.common.model.AysPage;
+import com.ays.common.model.dto.response.AysPageResponse;
+import com.ays.common.model.dto.response.AysResponse;
+import com.ays.common.model.dto.response.AysResponseBuilder;
+import com.ays.common.util.exception.model.AysError;
+import com.ays.util.AysMockMvcRequestBuilders;
+import com.ays.util.AysMockResultMatchersBuilders;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.List;
 
 class AdminUserRegisterApplicationControllerTest extends AbstractRestControllerTest {
 
     @MockBean
     private AdminUserRegisterApplicationService adminUserRegisterApplicationService;
+
+
+    private final AdminUserRegisterApplicationEntityToAdminUserRegisterApplicationMapper adminUserRegisterApplicationEntityToAdminUserRegisterApplicationMapper = AdminUserRegisterApplicationEntityToAdminUserRegisterApplicationMapper.initialize();
+    private final AdminUserRegisterApplicationToAdminUserRegisterApplicationsResponseMapper adminUserRegisterApplicationToAdminUserRegisterApplicationsResponseMapper = AdminUserRegisterApplicationToAdminUserRegisterApplicationsResponseMapper.initialize();
+
+
+    private static final String BASE_PATH = "/api/v1/admin";
+
+
+    @Test
+    void givenValidAdminUserRegisterApplicationListRequest_whenAdminUserRegisterApplicationsFound_thenReturnAysPageResponseOfAdminUserRegisterApplicationsResponse() throws Exception {
+
+        // Given
+        AdminUserRegisterApplicationListRequest mockListRequest = new AdminUserRegisterApplicationListRequestBuilder()
+                .withValidValues().build();
+
+        // When
+        List<AdminUserRegisterApplicationEntity> mockEntities = List.of(new AdminUserRegisterApplicationEntityBuilder().withValidFields().build());
+        Page<AdminUserRegisterApplicationEntity> mockPageEntities = new PageImpl<>(mockEntities);
+        List<AdminUserRegisterApplication> mockList = adminUserRegisterApplicationEntityToAdminUserRegisterApplicationMapper.map(mockEntities);
+        AysPage<AdminUserRegisterApplication> mockAysPage = AysPage
+                .of(mockListRequest.getFilter(), mockPageEntities, mockList);
+
+        Mockito.when(adminUserRegisterApplicationService.getRegistrationApplications(Mockito.any(AdminUserRegisterApplicationListRequest.class)))
+                .thenReturn(mockAysPage);
+
+        // Then
+        String endpoint = BASE_PATH.concat("/registration-applications");
+        List<AdminUserRegisterApplicationsResponse> mockResponse = adminUserRegisterApplicationToAdminUserRegisterApplicationsResponseMapper.map(mockList);
+        AysPageResponse<AdminUserRegisterApplicationsResponse> pageOfResponse = AysPageResponse.<AdminUserRegisterApplicationsResponse>builder()
+                .of(mockAysPage)
+                .content(mockResponse)
+                .build();
+
+        AysResponse<AysPageResponse<AdminUserRegisterApplicationsResponse>> mockAysResponse = AysResponse.successOf(pageOfResponse);
+        mockMvc.perform(AysMockMvcRequestBuilders
+                        .post(endpoint, mockSuperAdminToken.getAccessToken(), mockListRequest))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(AysMockResultMatchersBuilders.status().isOk())
+                .andExpect(AysMockResultMatchersBuilders.time().isNotEmpty())
+                .andExpect(AysMockResultMatchersBuilders.httpStatus().value(mockAysResponse.getHttpStatus().getReasonPhrase()))
+                .andExpect(AysMockResultMatchersBuilders.isSuccess().value(mockAysResponse.getIsSuccess()))
+                .andExpect(AysMockResultMatchersBuilders.response().isNotEmpty());
+
+        Mockito.verify(adminUserRegisterApplicationService, Mockito.times(1))
+                .getRegistrationApplications(Mockito.any(AdminUserRegisterApplicationListRequest.class));
+    }
+
+    @Test
+    void givenValidAdminUserRegisterApplicationListRequest_whenUserUnauthorizedForListing_thenReturnAccessDeniedException() throws Exception {
+        // Given
+        AdminUserRegisterApplicationListRequest mockListRequest = new AdminUserRegisterApplicationListRequestBuilder().withValidValues().build();
+
+        // Then
+        String endpoint = BASE_PATH.concat("/registration-applications");
+        AysResponse<AysError> mockResponse = AysResponseBuilder.FORBIDDEN;
+
+        mockMvc.perform(AysMockMvcRequestBuilders
+                        .post(endpoint, mockUserToken.getAccessToken(), mockListRequest))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(AysMockResultMatchersBuilders.status().isForbidden())
+                .andExpect(AysMockResultMatchersBuilders.time().isNotEmpty())
+                .andExpect(AysMockResultMatchersBuilders.httpStatus().value(mockResponse.getHttpStatus().name()))
+                .andExpect(AysMockResultMatchersBuilders.isSuccess().value(mockResponse.getIsSuccess()))
+                .andExpect(AysMockResultMatchersBuilders.response().doesNotExist());
+    }
+
 }
