9e41658c71f1234781f4041d081d539c84ae44f4
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-12-08 20:34:40 +0300

    Create AysPhoneNumberAccessor

---
+++ src/main/java/com/ays/common/model/AysPhoneNumberAccessor.java
@@ -0,0 +1,10 @@
+package com.ays.common.model;
+
+/**
+ * An interface to abstract the getters of the phone number classes.
+ */
+public interface AysPhoneNumberAccessor {
+    String getCountryCode();
+
+    String getLineNumber();
+}
