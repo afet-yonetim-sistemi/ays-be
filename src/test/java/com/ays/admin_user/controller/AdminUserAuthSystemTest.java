1a236c0dd865ca089a7e7463f79905bea9714766
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-12-23 18:48:34 +0300

    Remove the register system tests from AdminUserAuthSystemTest

--- src/test/java/com/ays/admin_user/controller/AdminUserAuthSystemTest.java
+++ src/test/java/com/ays/admin_user/controller/AdminUserAuthSystemTest.java
@@ -1,20 +1,15 @@
 package com.ays.admin_user.controller;
 
 import com.ays.AbstractSystemTest;
-import com.ays.admin_user.model.dto.request.AdminUserRegisterApplicationCompleteRequest;
-import com.ays.admin_user.model.dto.request.AdminUserRegisterRequestBuilder;
 import com.ays.auth.model.dto.request.AysLoginRequest;
 import com.ays.auth.model.dto.request.AysLoginRequestBuilder;
 import com.ays.auth.model.dto.request.AysTokenInvalidateRequest;
 import com.ays.auth.model.dto.request.AysTokenRefreshRequest;
 import com.ays.auth.model.dto.response.AysTokenResponse;
 import com.ays.auth.model.dto.response.AysTokenResponseBuilder;
-import com.ays.common.model.dto.request.AysPhoneNumberRequest;
-import com.ays.common.model.dto.request.AysPhoneNumberRequestBuilder;
 import com.ays.common.model.dto.response.AysResponse;
 import com.ays.common.model.dto.response.AysResponseBuilder;
 import com.ays.common.util.exception.model.AysError;
-import com.ays.common.util.exception.model.AysErrorBuilder;
 import com.ays.util.AysMockMvcRequestBuilders;
 import com.ays.util.AysMockResultMatchersBuilders;
 import com.ays.util.AysValidTestData;
@@ -26,122 +21,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 class AdminUserAuthSystemTest extends AbstractSystemTest {
 
     private static final String BASE_PATH = "/api/v1/authentication/admin";
-
-    @Test
-    void givenValidAdminUserRegisterRequest_whenAdminUserRegistered_thenReturnSuccessResponse() throws Exception {
-        // Given
-        AdminUserRegisterApplicationCompleteRequest registerRequest = new AdminUserRegisterRequestBuilder()
-                .withValidFields()
-                .withInstitutionId(AysValidTestData.Institution.ID)
-                .withApplicationId(AysValidTestData.APPLICATION_ID)
-                .build();
-
-        // Then
-        AysResponse<Void> response = AysResponseBuilder.SUCCESS;
-        mockMvc.perform(AysMockMvcRequestBuilders
-                        .post(BASE_PATH.concat("/register"), registerRequest))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(AysMockResultMatchersBuilders.status().isOk())
-                .andExpect(AysMockResultMatchersBuilders.time()
-                        .isNotEmpty())
-                .andExpect(AysMockResultMatchersBuilders.httpStatus()
-                        .value(response.getHttpStatus().name()))
-                .andExpect(AysMockResultMatchersBuilders.isSuccess()
-                        .value(response.getIsSuccess()))
-                .andExpect(AysMockResultMatchersBuilders.response()
-                        .doesNotExist());
-    }
-
-    @Test
-    void givenPhoneNumberWithAlphanumericCharacter_whenPhoneNumberIsNotValid_thenReturnValidationError() throws Exception {
-        // Given
-        AysPhoneNumberRequest phoneNumber = new AysPhoneNumberRequestBuilder()
-                .withCountryCode("ABC")
-                .withLineNumber("ABC").build();
-        AdminUserRegisterApplicationCompleteRequest registerRequest = new AdminUserRegisterRequestBuilder()
-                .withValidFields()
-                .withPhoneNumber(phoneNumber).build();
-
-        // Then
-        AysError errorResponse = AysErrorBuilder.VALIDATION_ERROR;
-        mockMvc.perform(AysMockMvcRequestBuilders
-                        .post(BASE_PATH.concat("/register"), registerRequest))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(AysMockResultMatchersBuilders.status().isBadRequest())
-                .andExpect(AysMockResultMatchersBuilders.time()
-                        .isNotEmpty())
-                .andExpect(AysMockResultMatchersBuilders.httpStatus()
-                        .value(errorResponse.getHttpStatus().name()))
-                .andExpect(AysMockResultMatchersBuilders.header()
-                        .value(errorResponse.getHeader()))
-                .andExpect(AysMockResultMatchersBuilders.isSuccess()
-                        .value(errorResponse.getIsSuccess()))
-                .andExpect(AysMockResultMatchersBuilders.response()
-                        .doesNotExist())
-                .andExpect(AysMockResultMatchersBuilders.subErrors()
-                        .isNotEmpty());
-    }
-
-    @Test
-    void givenPhoneNumberWithInvalidLength_whenPhoneNumberIsNotValid_thenReturnValidationError() throws Exception {
-        // Given
-        AysPhoneNumberRequest phoneNumber = new AysPhoneNumberRequestBuilder()
-                .withCountryCode("456786745645")
-                .withLineNumber("6546467456435548676845321346656654").build();
-        AdminUserRegisterApplicationCompleteRequest registerRequest = new AdminUserRegisterRequestBuilder()
-                .withValidFields()
-                .withPhoneNumber(phoneNumber).build();
-
-        // Then
-        AysError errorResponse = AysErrorBuilder.VALIDATION_ERROR;
-        mockMvc.perform(AysMockMvcRequestBuilders
-                        .post(BASE_PATH.concat("/register"), registerRequest))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(AysMockResultMatchersBuilders.status().isBadRequest())
-                .andExpect(AysMockResultMatchersBuilders.time()
-                        .isNotEmpty())
-                .andExpect(AysMockResultMatchersBuilders.httpStatus()
-                        .value(errorResponse.getHttpStatus().name()))
-                .andExpect(AysMockResultMatchersBuilders.header()
-                        .value(errorResponse.getHeader()))
-                .andExpect(AysMockResultMatchersBuilders.isSuccess()
-                        .value(errorResponse.getIsSuccess()))
-                .andExpect(AysMockResultMatchersBuilders.response()
-                        .doesNotExist())
-                .andExpect(AysMockResultMatchersBuilders.subErrors()
-                        .isNotEmpty());
-    }
-
-    @Test
-    void givenPhoneNumberWithInvalidOperator_whenPhoneNumberIsNotValid_thenReturnValidationError() throws Exception {
-        // Given
-        final String invalidOperator = "123";
-        AysPhoneNumberRequest mockPhoneNumber = new AysPhoneNumberRequestBuilder()
-                .withCountryCode("90")
-                .withLineNumber(invalidOperator + "6327218").build();
-        AdminUserRegisterApplicationCompleteRequest registerRequest = new AdminUserRegisterRequestBuilder()
-                .withValidFields()
-                .withPhoneNumber(mockPhoneNumber).build();
-
-        // Then
-        AysError errorResponse = AysErrorBuilder.VALIDATION_ERROR;
-        mockMvc.perform(AysMockMvcRequestBuilders
-                        .post(BASE_PATH.concat("/register"), registerRequest))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(AysMockResultMatchersBuilders.status().isBadRequest())
-                .andExpect(AysMockResultMatchersBuilders.time()
-                        .isNotEmpty())
-                .andExpect(AysMockResultMatchersBuilders.httpStatus()
-                        .value(errorResponse.getHttpStatus().name()))
-                .andExpect(AysMockResultMatchersBuilders.header()
-                        .value(errorResponse.getHeader()))
-                .andExpect(AysMockResultMatchersBuilders.isSuccess()
-                        .value(errorResponse.getIsSuccess()))
-                .andExpect(AysMockResultMatchersBuilders.response()
-                        .doesNotExist())
-                .andExpect(AysMockResultMatchersBuilders.subErrors()
-                        .isNotEmpty());
-    }
 
     @Test
     void givenValidLoginRequest_whenTokensGeneratedSuccessfully_thenReturnTokenResponse() throws Exception {
