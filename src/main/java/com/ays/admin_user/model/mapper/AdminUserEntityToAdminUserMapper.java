2d4d2b4c50e7a6faca194ceeaf2c54d70f929d5c
Sercan Noyan Germiyanoğlu <sngermiyanoglu@hotmail.com>
2024-01-07 13:07:22 +0800

    AYS-2 | Fix the issue not showing phone number info in AdminUserRegisterApplicationResponse and Add Some Java Docs in Some Mapper Classes (#279)

--- src/main/java/com/ays/admin_user/model/mapper/AdminUserEntityToAdminUserMapper.java
+++ src/main/java/com/ays/admin_user/model/mapper/AdminUserEntityToAdminUserMapper.java
@@ -7,6 +7,14 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+/**
+ * AdminUserEntityToAdminUserMapper is an interface that defines the
+ * mapping between an {@link AdminUserEntity} and an {@link AdminUser}.
+ * This interface uses the MapStruct annotation @Mapper to generate an implementation of this interface at compile-time.
+ * <p>The class provides a static method {@code initialize()} that returns an instance of the generated mapper implementation.
+ * <p>The interface extends the MapStruct interface {@link BaseMapper}, which defines basic mapping methods.
+ * The interface adds no additional mapping methods, but simply defines the types to be used in the mapping process.
+ */
 @Mapper
 public interface AdminUserEntityToAdminUserMapper extends BaseMapper<AdminUserEntity, AdminUser> {
 
@@ -16,7 +24,13 @@ public interface AdminUserEntityToAdminUserMapper extends BaseMapper<AdminUserEn
     @Mapping(target = "phoneNumber.lineNumber", source = "source.lineNumber")
     AdminUser map(AdminUserEntity source);
 
+    /**
+     * Initializes the mapper.
+     *
+     * @return the initialized mapper object.
+     */
     static AdminUserEntityToAdminUserMapper initialize() {
         return Mappers.getMapper(AdminUserEntityToAdminUserMapper.class);
     }
+
 }
