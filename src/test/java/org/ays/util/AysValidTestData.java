652c6a1665d268d1504c8f233d13c3249fe63733
agitrubard <demiragitrubar@gmail.com>
2024-05-11 09:38:31 +0300

    AYS-199 | `SuperAdminUserV2` and `UserV2` Classes Has Been Created in `AysValidTestData` Class

--- src/test/java/org/ays/util/AysValidTestData.java
+++ src/test/java/org/ays/util/AysValidTestData.java
@@ -9,6 +9,17 @@ public class AysValidTestData {
     public static final String PASSWORD = "A123y456S.";
     public static final String PASSWORD_ENCRYPTED = "$2a$10$H/lKEaKsusQztOaJmYTAi.4MAmjvnxWOh0DY.XrgwHy5D2gENVIky";
 
+    public static class SuperAdminUserV2 {
+        public static final String ID = "f882cb9c-9341-473b-a040-3fbd05c09ac6";
+        public static final String EMAIL_ADDRESS = "james.william@afetyonetimsistemi.org";
+    }
+
+    public static class UserV2 {
+        public static final String ID = "cf1587fd-f800-42f2-ac6e-bd7b1c4d993d";
+        public static final String INSTITUTION_ID = "08cd4b85-bb28-4e4a-a322-fb7b293d97f8";
+        public static final String EMAIL_ADDRESS = "diego.ruiz@afetyonetimsistemi.org";
+    }
+
     public static class SuperAdminUser {
         public static final String ID = "ea29a0bd-ad5d-4068-88c3-a8835b8d555e";
         public static final String USERNAME = "ays-super-admin";
