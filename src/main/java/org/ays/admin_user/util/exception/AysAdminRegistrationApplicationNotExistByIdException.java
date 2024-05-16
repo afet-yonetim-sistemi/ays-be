fd25751bf530e6f5ef5662e38977569b40b84dee
agitrubard <demiragitrubar@gmail.com>
2024-05-16 17:11:24 +0300

    AYS-246 | `AysAdminRegisterApplicationNotExistByIdException` Class Has Been Renamed as `AysAdminRegistrationApplicationNotExistByIdException`

--- src/main/java/org/ays/admin_user/util/exception/AysAdminRegistrationApplicationNotExistByIdException.java
+++ src/main/java/org/ays/admin_user/util/exception/AysAdminRegistrationApplicationNotExistByIdException.java
@@ -5,7 +5,7 @@ import org.ays.common.util.exception.AysNotExistException;
 import java.io.Serial;
 
 /**
- * Exception indicating that an admin register application does not exist with the specified ID.
+ * Exception indicating that an admin registration application does not exist with the specified ID.
  * This exception is a subclass of AysNotExistException, which is typically used to indicate that an entity or
  * resource does not exist.
  * Typically, this exception is thrown when an operation or query is performed on an admin register application
@@ -20,12 +20,12 @@ public class AysAdminRegistrationApplicationNotExistByIdException extends AysNot
     private static final long serialVersionUID = 8416712253227498925L;
 
     /**
-     * Constructs a new AysAdminUserRegisterApplicationNotExistByIdException with the specified ID.
+     * Constructs a new {@link AysAdminRegistrationApplicationNotExistByIdException} with the specified ID.
      *
-     * @param id The ID of the admin register application that does not exist.
+     * @param id The ID of the admin registration application that does not exist.
      */
     public AysAdminRegistrationApplicationNotExistByIdException(String id) {
-        super("ADMIN REGISTER APPLICATION NOT EXIST! id:" + id);
+        super("ADMIN REGISTRATION APPLICATION NOT EXIST! id:" + id);
     }
 
 }
