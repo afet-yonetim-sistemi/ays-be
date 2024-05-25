619eaedd3be3da1828dbc1a23cd0ada2d26f3a0f
agitrubard <demiragitrubar@gmail.com>
2024-05-25 16:45:33 +0300

    AYS-221 | `EmergencyEvacuationApplicationRequest` Validations Have Been Covered with Integration Tests

--- src/test/java/org/ays/emergency_application/model/dto/request/EmergencyEvacuationRequestBuilder.java
+++ src/test/java/org/ays/emergency_application/model/dto/request/EmergencyEvacuationRequestBuilder.java
@@ -11,12 +11,74 @@ public class EmergencyEvacuationRequestBuilder extends TestDataBuilder<Emergency
 
     public EmergencyEvacuationRequestBuilder withValidFields() {
         return this
-                .withPhoneNumber(new AysPhoneNumberRequestBuilder().withValidFields().build());
+                .withPhoneNumber(new AysPhoneNumberRequestBuilder().withValidFields().build())
+                .withApplicantPhoneNumber(new AysPhoneNumberRequestBuilder().withValidFields().build());
+    }
+
+    public EmergencyEvacuationRequestBuilder withFirstName(String firstName) {
+        data.setFirstName(firstName);
+        return this;
+    }
+
+    public EmergencyEvacuationRequestBuilder withLastName(String lastName) {
+        data.setLastName(lastName);
+        return this;
     }
 
     public EmergencyEvacuationRequestBuilder withPhoneNumber(AysPhoneNumberRequest phoneNumber) {
         data.setPhoneNumber(phoneNumber);
+        return this;
+    }
+
+    public EmergencyEvacuationRequestBuilder withSourceCity(String sourceCity) {
+        data.setSourceCity(sourceCity);
+        return this;
+    }
+
+    public EmergencyEvacuationRequestBuilder withSourceDistrict(String sourceDistrict) {
+        data.setSourceDistrict(sourceDistrict);
+        return this;
+    }
+
+    public EmergencyEvacuationRequestBuilder withAddress(String address) {
+        data.setAddress(address);
+        return this;
+    }
+
+    public EmergencyEvacuationRequestBuilder withSeatingCount(Integer seatingCount) {
+        data.setSeatingCount(seatingCount);
+        return this;
+    }
+
+    public EmergencyEvacuationRequestBuilder withTargetCity(String targetCity) {
+        data.setTargetCity(targetCity);
+        return this;
+    }
+
+    public EmergencyEvacuationRequestBuilder withTargetDistrict(String targetDistrict) {
+        data.setTargetDistrict(targetDistrict);
+        return this;
+    }
+
+    public EmergencyEvacuationRequestBuilder withApplicantFirstName(String applicantFirstName) {
+        data.setApplicantFirstName(applicantFirstName);
+        return this;
+    }
+
+    public EmergencyEvacuationRequestBuilder withApplicantLastName(String applicantLastName) {
+        data.setApplicantLastName(applicantLastName);
+        return this;
+    }
+
+    public EmergencyEvacuationRequestBuilder withApplicantPhoneNumber(AysPhoneNumberRequest phoneNumber) {
         data.setApplicantPhoneNumber(phoneNumber);
+        return this;
+    }
+
+    public EmergencyEvacuationRequestBuilder withoutApplicant() {
+        data.setApplicantFirstName(null);
+        data.setApplicantLastName(null);
+        data.setApplicantPhoneNumber(null);
         return this;
     }
 
