fa2e8508b431750344a6c34b46c6cc2770ef482e
agitrubard <demiragitrubar@gmail.com>
2024-05-25 15:38:10 +0300

    AYS-221 | `EmergencyEvacuationApplicationStatus` Class Has Been Refactored

--- src/main/java/org/ays/emergency_application/model/entity/EmergencyEvacuationApplicationStatus.java
+++ src/main/java/org/ays/emergency_application/model/entity/EmergencyEvacuationApplicationStatus.java
@@ -4,19 +4,14 @@ package org.ays.emergency_application.model.entity;
  * Enumeration keeping emergency evacuation status.
  */
 public enum EmergencyEvacuationApplicationStatus {
+
     PENDING,
-
     IN_REVIEW,
-
     IN_PROGRESS,
-
     RECEIVED_FIRST_APPROVE,
-
     RECEIVED_SECOND_APPROVE,
-
     RECEIVED_THIRD_APPROVE,
-
     COMPLETED,
-
     CANCELLED
+
 }
