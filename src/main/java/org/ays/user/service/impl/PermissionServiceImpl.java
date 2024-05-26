e240ffba06ae0be4519807531ec0783ed4d108cd
agitrubard <demiragitrubar@gmail.com>
2024-05-26 18:10:52 +0300

    AYS-236 | Permission List Flow Has Been Created End to End

---
+++ src/main/java/org/ays/user/service/impl/PermissionServiceImpl.java
@@ -0,0 +1,54 @@
+package org.ays.user.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.ays.auth.model.AysIdentity;
+import org.ays.user.model.Permission;
+import org.ays.user.model.entity.PermissionEntity;
+import org.ays.user.model.mapper.PermissionEntityToPermissionMapper;
+import org.ays.user.repository.PermissionRepository;
+import org.ays.user.service.PermissionService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Service implementation for managing permissions.
+ * <p>
+ * The {@code PermissionServiceImpl} class provides the implementation for the {@link PermissionService}
+ * interface, handling operations related to permissions. It interacts with the {@link PermissionRepository}
+ * to retrieve permission data and uses a mapper to convert entities to domain models.
+ * </p>
+ */
+@Service
+@RequiredArgsConstructor
+class PermissionServiceImpl implements PermissionService {
+
+    private final PermissionRepository permissionRepository;
+
+    private final AysIdentity identity;
+
+    private final PermissionEntityToPermissionMapper permissionEntityToPermissionMapper = PermissionEntityToPermissionMapper.initialize();
+
+    /**
+     * Retrieves all permissions available in the system.
+     * <p>
+     * If the current user is a super admin, all permissions are returned.
+     * Otherwise, only non-super permissions are returned.
+     * </p>
+     *
+     * @return a list of {@link Permission} objects representing the permissions.
+     */
+    @Override
+    public List<Permission> findAll() {
+
+        if (identity.isSuperAdmin()) {
+            final List<PermissionEntity> permissionEntities = permissionRepository.findAll();
+            return permissionEntityToPermissionMapper.map(permissionEntities);
+        }
+
+        Set<PermissionEntity> permissionEntities = permissionRepository.findAllByIsSuperFalse(); // TODO : set yerine list kullanılmalı
+        return permissionEntityToPermissionMapper.map(permissionEntities);
+    }
+
+}
