488f1eca0af946390124d278ac55d3559d3ca80c
agitrubard <demiragitrubar@gmail.com>
2024-06-03 19:09:26 +0300

    AYS-246 | Import Has Been Fixed

--- src/main/java/org/ays/user/repository/RoleRepository.java
+++ src/main/java/org/ays/user/repository/RoleRepository.java
@@ -3,6 +3,7 @@ package org.ays.user.repository;
 import org.ays.user.model.entity.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.Set;
 
 /**
