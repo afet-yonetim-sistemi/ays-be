4342dd84d613068934a63cb07aa0ad27b32c6bd0
Egehan Asal <egehanasal.dev@gmail.com>
2024-06-07 18:36:01 +0300

    AYS-223 | Javadoc Has Been Added to EmergencyEvacuationApplicationToApplicationsDetailResponseMapper Class

--- src/main/java/org/ays/emergency_application/model/mapper/EmergencyEvacuationApplicationToApplicationsDetailResponseMapper.java
+++ src/main/java/org/ays/emergency_application/model/mapper/EmergencyEvacuationApplicationToApplicationsDetailResponseMapper.java
@@ -6,12 +6,18 @@ import org.ays.emergency_application.model.dto.response.EmergencyEvacuationAppli
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-// TODO AYS-223 : Add Javadoc
+/**
+ * EmergencyEvacuationRequestToEmergencyEvacuationEntityMapper is an interface that defines the
+ * mapping between an {@link EmergencyEvacuationApplication} and an {@link EmergencyEvacuationApplicationDetailResponse}.
+ * This interface uses the MapStruct annotation @Mapper to generate an implementation of this interface at compile-time.
+ * <p>The class provides a static method {@code initialize()} that returns an instance of the generated mapper implementation.
+ * <p>The interface extends the MapStruct interface {@link BaseMapper}, which defines basic mapping methods.
+ * The interface adds no additional mapping methods, but simply defines the types to be used in the mapping process.
+ */
 @Mapper
 public interface EmergencyEvacuationApplicationToApplicationsDetailResponseMapper extends BaseMapper<EmergencyEvacuationApplication, EmergencyEvacuationApplicationDetailResponse> {
 
     static EmergencyEvacuationApplicationToApplicationsDetailResponseMapper initialize() {
         return Mappers.getMapper(EmergencyEvacuationApplicationToApplicationsDetailResponseMapper.class);
     }
-
 }
