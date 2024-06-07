00ae28fedb740037d7593d43963e1f3ff4d9032a
Egehan Asal <egehanasal.dev@gmail.com>
2024-06-07 20:01:15 +0300

    AYS-223 | Javadoc Has Been Fixed

--- src/main/java/org/ays/emergency_application/model/mapper/EmergencyEvacuationApplicationToApplicationsDetailResponseMapper.java
+++ src/main/java/org/ays/emergency_application/model/mapper/EmergencyEvacuationApplicationToApplicationsDetailResponseMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 /**
- * EmergencyEvacuationRequestToEmergencyEvacuationEntityMapper is an interface that defines the
+ * {@link EmergencyEvacuationApplicationToApplicationsDetailResponseMapper} is an interface that defines the
  * mapping between an {@link EmergencyEvacuationApplication} and an {@link EmergencyEvacuationApplicationDetailResponse}.
  * This interface uses the MapStruct annotation @Mapper to generate an implementation of this interface at compile-time.
  * <p>The class provides a static method {@code initialize()} that returns an instance of the generated mapper implementation.
