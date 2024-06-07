d75450639d202ead3cefc07e81f3a2cb816feaa7
agitrubard <demiragitrubar@gmail.com>
2024-06-07 15:05:00 +0300

    AYS-222 | Javadoc Has Been Added to Emergency Evacuation Application Mapper Interfaces

--- src/main/java/org/ays/emergency_application/model/mapper/EmergencyEvacuationApplicationToApplicationsResponseMapper.java
+++ src/main/java/org/ays/emergency_application/model/mapper/EmergencyEvacuationApplicationToApplicationsResponseMapper.java
@@ -6,7 +6,14 @@ import org.ays.emergency_application.model.dto.response.EmergencyEvacuationAppli
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-// TODO AYS-222 : Add Javadoc
+/**
+ * {@link EmergencyEvacuationApplicationToApplicationsResponseMapper} is an interface that defines the
+ * mapping between an {@link EmergencyEvacuationApplication} and an {@link EmergencyEvacuationApplicationsResponse}.
+ * This interface uses the MapStruct annotation @Mapper to generate an implementation of this interface at compile-time.
+ * <p>The class provides a static method {@code initialize()} that returns an instance of the generated mapper implementation.
+ * <p>The interface extends the MapStruct interface {@link BaseMapper}, which defines basic mapping methods.
+ * The interface adds no additional mapping methods, but simply defines the types to be used in the mapping process.
+ */
 @Mapper
 public interface EmergencyEvacuationApplicationToApplicationsResponseMapper extends BaseMapper<EmergencyEvacuationApplication, EmergencyEvacuationApplicationsResponse> {
 
