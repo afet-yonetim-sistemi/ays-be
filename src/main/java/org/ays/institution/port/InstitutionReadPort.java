402e86689ebccc45dfb3836c8c576f71cbb30f7b
agitrubard <demiragitrubar@gmail.com>
2024-06-16 10:22:01 +0300

    AYS-275 | Javadoc Has Been Updated in `InstitutionReadPort` Class

--- src/main/java/org/ays/institution/port/InstitutionReadPort.java
+++ src/main/java/org/ays/institution/port/InstitutionReadPort.java
@@ -2,16 +2,21 @@ package org.ays.institution.port;
 
 import org.ays.institution.model.Institution;
 import org.ays.institution.model.enums.InstitutionStatus;
-import org.ays.parameter.model.AysParameter;
 
 import java.util.List;
 
 /**
- * A read port interface for accessing {@link AysParameter} data.
- * Defines methods to retrieve {@link AysParameter} entities based on name or name prefix.
+ * A read port interface for accessing {@link Institution} data.
+ * Defines methods to retrieve {@link Institution} entities based on status and to check if an institution exists by ID and status.
  */
 public interface InstitutionReadPort {
 
+    /**
+     * Retrieves a list of all {@link Institution} entities with the specified status, ordered by name in ascending order.
+     *
+     * @param status the status of the institutions to retrieve
+     * @return a list of {@link Institution} entities with the specified status
+     */
     List<Institution> findAllByStatusOrderByNameAsc(InstitutionStatus status);
 
 }
