f95bbc71b896a50f5b3888c815ee92321a0688df
agitrubard <demiragitrubar@gmail.com>
2024-06-16 10:18:39 +0300

    AYS-275 | Institution Service Layer Has Been Covered with Unit Tests

--- src/test/java/org/ays/institution/service/impl/InstitutionServiceImplTest.java
+++ src/test/java/org/ays/institution/service/impl/InstitutionServiceImplTest.java
@@ -3,18 +3,15 @@ package org.ays.institution.service.impl;
 import org.apache.commons.collections4.CollectionUtils;
 import org.ays.AbstractUnitTest;
 import org.ays.institution.model.Institution;
-import org.ays.institution.model.entity.InstitutionEntity;
-import org.ays.institution.model.entity.InstitutionEntityBuilder;
+import org.ays.institution.model.entity.InstitutionBuilder;
 import org.ays.institution.model.enums.InstitutionStatus;
-import org.ays.institution.model.mapper.InstitutionEntityToInstitutionMapper;
-import org.ays.institution.repository.InstitutionRepository;
+import org.ays.institution.port.InstitutionReadPort;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,48 +22,50 @@ class InstitutionServiceImplTest extends AbstractUnitTest {
     private InstitutionServiceImpl institutionService;
 
     @Mock
-    private InstitutionRepository institutionRepository;
+    private InstitutionReadPort institutionReadPort;
 
-    private final InstitutionEntityToInstitutionMapper institutionEntityToInstitutionMapper = InstitutionEntityToInstitutionMapper.initialize();
 
     @Test
     void whenActiveInstitutionsExist_thenReturnInstitutions() {
 
         // When
-        List<InstitutionEntity> mockActiveInstitutionEntities = Arrays.asList(
-                new InstitutionEntityBuilder().withValidFields().build(),
-                new InstitutionEntityBuilder().withValidFields().build()
+        InstitutionStatus status = InstitutionStatus.ACTIVE;
+
+        List<Institution> mockActiveInstitutions = List.of(
+                new InstitutionBuilder().withValidFields().withStatus(status).build(),
+                new InstitutionBuilder().withValidFields().withStatus(status).build()
         );
 
-        Mockito.when(institutionRepository.findAllByStatusOrderByNameAsc(InstitutionStatus.ACTIVE))
-                .thenReturn(mockActiveInstitutionEntities);
+        Mockito.when(institutionReadPort.findAllByStatusOrderByNameAsc(status))
+                .thenReturn(mockActiveInstitutions);
 
         // Then
-        List<Institution> mockActiveInstitutions = institutionEntityToInstitutionMapper
-                .map(mockActiveInstitutionEntities);
         List<Institution> activeInstitutions = institutionService.getSummaryOfActiveInstitutions();
 
         Assertions.assertNotNull(activeInstitutions);
         Assertions.assertEquals(activeInstitutions.size(), mockActiveInstitutions.size());
 
-        Mockito.verify(institutionRepository, Mockito.times(1))
-                .findAllByStatusOrderByNameAsc(InstitutionStatus.ACTIVE);
-
+        // Verify
+        Mockito.verify(institutionReadPort, Mockito.times(1))
+                .findAllByStatusOrderByNameAsc(status);
     }
 
     @Test
     void whenActiveInstitutionsNotExist_thenReturnEmptyList() {
 
         // When
-        Mockito.when(institutionRepository.findAllByStatusOrderByNameAsc(InstitutionStatus.ACTIVE))
+        InstitutionStatus status = InstitutionStatus.ACTIVE;
+
+        Mockito.when(institutionReadPort.findAllByStatusOrderByNameAsc(status))
                 .thenReturn(Collections.emptyList());
         // Then
         List<Institution> institutions = institutionService.getSummaryOfActiveInstitutions();
 
         Assertions.assertTrue(CollectionUtils.isEmpty(institutions));
 
-        Mockito.verify(institutionRepository, Mockito.times(1))
-                .findAllByStatusOrderByNameAsc(InstitutionStatus.ACTIVE);
+        // Verify
+        Mockito.verify(institutionReadPort, Mockito.times(1))
+                .findAllByStatusOrderByNameAsc(status);
 
     }
 
