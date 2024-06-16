f253733099243cc42f6b63b7c0e16eb2d9e86281
agitrubard <demiragitrubar@gmail.com>
2024-06-16 07:16:02 +0300

    AYS-275 | Descriptions of Lombok Annotation Have Been Removed from Javadoc in `AysTokenResponse` Class

--- src/main/java/org/ays/auth/model/response/AysTokenResponse.java
+++ src/main/java/org/ays/auth/model/response/AysTokenResponse.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 /**
  * AysTokenResponse is a POJO class that represents a response object containing the access token, its expiration time, and the refresh token.
  * This object is typically returned by an authentication endpoint after the user successfully logs in or refreshes their token.
- * <p>The class uses the Lombok annotations @Data and @Builder to automatically generate getters, setters, and a builder method.
  */
 @Getter
 @Setter
