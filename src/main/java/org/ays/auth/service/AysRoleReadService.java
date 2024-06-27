d09b067fc2b271273a54505e46184cd3b58c4f22
agitrubard <demiragitrubar@gmail.com>
2024-06-27 11:33:30 +0300

    AYS-237 | `AysRoleReadService` Layer and `AysRoleReadService.findAll()` Method Has Been Created

---
+++ src/main/java/org/ays/auth/service/AysRoleReadService.java
@@ -0,0 +1,35 @@
+package org.ays.auth.service;
+
+import org.ays.auth.model.AysRole;
+import org.ays.auth.model.request.AysRoleListRequest;
+import org.ays.common.model.AysPage;
+
+/**
+ * Service interface for reading and retrieving roles in a paginated format.
+ * <p>
+ * The {@link AysRoleReadService} provides a method for retrieving a paginated list of roles
+ * based on the specified request parameters. Implementations of this interface should handle
+ * the retrieval of roles, including any necessary filtering, sorting, and pagination logic
+ * as defined in the {@link AysRoleListRequest}.
+ * </p>
+ *
+ * @see AysRole
+ * @see AysRoleListRequest
+ * @see AysPage
+ */
+public interface AysRoleReadService {
+
+    /**
+     * Retrieves a paginated list of roles based on the specified {@link AysRoleListRequest}.
+     * <p>
+     * This method handles the retrieval of roles from a data source according to the parameters
+     * defined in the {@code listRequest}. It typically involves filtering, sorting, and paginating
+     * the results to match the request criteria.
+     * </p>
+     *
+     * @param listRequest the request containing parameters for filtering, sorting, and pagination.
+     * @return a paginated list of roles matching the request criteria.
+     */
+    AysPage<AysRole> findAll(AysRoleListRequest listRequest);
+
+}
---
+++ src/main/java/org/ays/auth/service/impl/AysRoleReadServiceImpl.java
@@ -0,0 +1,66 @@
+package org.ays.auth.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.ays.auth.model.AysIdentity;
+import org.ays.auth.model.AysRole;
+import org.ays.auth.model.AysRoleFilter;
+import org.ays.auth.model.request.AysRoleListRequest;
+import org.ays.auth.port.AysRoleReadPort;
+import org.ays.auth.service.AysRoleReadService;
+import org.ays.common.model.AysPage;
+import org.ays.common.model.AysPageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+/**
+ * Service implementation for reading roles.
+ * <p>
+ * The {@code AysRoleReadServiceImpl} class provides the logic for retrieving roles based on the provided request
+ * and ensures that the roles retrieved are scoped to the institution of the current user.
+ * </p>
+ */
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+class AysRoleReadServiceImpl implements AysRoleReadService {
+
+    private final AysRoleReadPort roleReadPort;
+
+    private final AysIdentity identity;
+
+
+    /**
+     * Retrieves a paginated list of roles based on the provided request.
+     * <p>
+     * If no filter is provided in the request, a default filter is applied which scopes the results
+     * to the institution associated with the current user.
+     * </p>
+     *
+     * @param listRequest the request containing pagination and filtering information.
+     * @return a paginated list of roles.
+     */
+    @Override
+    public AysPage<AysRole> findAll(final AysRoleListRequest listRequest) {
+
+        final AysPageable aysPageable = listRequest.getPageable();
+
+        Optional.ofNullable(listRequest.getFilter())
+                .ifPresentOrElse(filter -> {
+                            if (filter.getInstitutionId() == null) {
+                                filter.setInstitutionId(identity.getInstitutionId());
+                            }
+                        },
+                        () -> {
+                            AysRoleFilter filter = AysRoleFilter.builder()
+                                    .institutionId(identity.getInstitutionId())
+                                    .build();
+
+                            listRequest.setFilter(filter);
+                        });
+
+        return roleReadPort.findAll(aysPageable, listRequest.getFilter());
+    }
+
+}
