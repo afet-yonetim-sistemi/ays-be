48cbd72841b8fda24da32a1b0e647ad49edc2f3b
agitrubard <demiragitrubar@gmail.com>
2024-06-27 11:32:44 +0300

    AYS-237 | `AysRoleReadPort.findAll()` Method Has Been Created

--- src/main/java/org/ays/auth/port/AysRoleReadPort.java
+++ src/main/java/org/ays/auth/port/AysRoleReadPort.java
@@ -1,14 +1,31 @@
 package org.ays.auth.port;
 
 import org.ays.auth.model.AysRole;
+import org.ays.auth.model.AysRoleFilter;
+import org.ays.common.model.AysPage;
+import org.ays.common.model.AysPageable;
 
 import java.util.Optional;
 import java.util.Set;
 
 /**
- * Port interface for reading operations related to active roles by institution ID and by name.
+ * {@link AysRoleReadPort} is an interface for reading role data from the underlying data source.
+ * <p>
+ * It provides methods for retrieving roles based on various criteria such as pagination, filters,
+ * institution ID, and role name. This interface is part of the application's hexagonal architecture,
+ * facilitating the separation between domain logic and data access logic.
+ * </p>
  */
 public interface AysRoleReadPort {
+
+    /**
+     * Retrieves a paginated list of roles based on the provided pageable and filter.
+     *
+     * @param aysPageable The pagination configuration.
+     * @param filter      The filter criteria for the roles.
+     * @return A paginated list of roles.
+     */
+    AysPage<AysRole> findAll(AysPageable aysPageable, AysRoleFilter filter);
 
     /**
      * Retrieves all active roles associated with a specific institution.
