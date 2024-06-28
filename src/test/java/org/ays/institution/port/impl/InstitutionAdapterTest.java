bfa959e5c2735f4d29894d8bf979997c2af68d78
agitrubard <demiragitrubar@gmail.com>
2024-06-28 16:33:14 +0300

    AYS-237 | `givenValidId_whenActiveInstitutinExist_thenReturnTrue()` Test Has Been Renamed as `givenValidId_whenActiveInstitutionExist_thenReturnTrue()`

--- src/test/java/org/ays/institution/port/impl/InstitutionAdapterTest.java
+++ src/test/java/org/ays/institution/port/impl/InstitutionAdapterTest.java
@@ -77,7 +77,7 @@ class InstitutionAdapterTest extends AysUnitTest {
 
 
     @Test
-    void givenValidId_whenActiveInstitutinExist_thenReturnTrue() {
+    void givenValidId_whenActiveInstitutionExist_thenReturnTrue() {
         // Given
         String mockId = AysRandomUtil.generateUUID();
 
