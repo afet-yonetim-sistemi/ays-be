a2567eda71605a45d998a2081619eb6abee7c8a3
agitrubard <demiragitrubar@gmail.com>
2024-06-29 13:33:17 +0300

    AYS-237 | `UserAdapter.findById()` Method Has Been Covered with Unit Tests

--- src/main/java/org/ays/common/model/AysPhoneNumber.java
+++ src/main/java/org/ays/common/model/AysPhoneNumber.java
@@ -1,6 +1,7 @@
 package org.ays.common.model;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@EqualsAndHashCode
 public class AysPhoneNumber {
 
     /**
--- src/test/java/org/ays/auth/port/impl/AysUserAdapterTest.java
+++ src/test/java/org/ays/auth/port/impl/AysUserAdapterTest.java
@@ -1,11 +1,20 @@
 package org.ays.auth.port.impl;
 
 import org.ays.AysUnitTest;
+import org.ays.auth.model.AysUser;
+import org.ays.auth.model.entity.AysUserEntity;
+import org.ays.auth.model.entity.AysUserEntityBuilder;
 import org.ays.auth.model.mapper.AysUserEntityToDomainMapper;
 import org.ays.auth.model.mapper.AysUserToEntityMapper;
 import org.ays.auth.repository.AysUserRepository;
+import org.ays.common.util.AysRandomUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Optional;
 
 class AysUserAdapterTest extends AysUnitTest {
 
@@ -18,5 +27,53 @@ class AysUserAdapterTest extends AysUnitTest {
 
     private final AysUserToEntityMapper userToEntityMapper = AysUserToEntityMapper.initialize();
     private final AysUserEntityToDomainMapper userEntityToDomainMapper = AysUserEntityToDomainMapper.initialize();
+
+
+    @Test
+    void givenValidId_whenUserFoundById_thenReturnOptionalUser() {
+
+        // Given
+        String mockId = AysRandomUtil.generateUUID();
+
+        // When
+        AysUserEntity mockUserEntity = new AysUserEntityBuilder()
+                .withValidValues()
+                .withId(mockId)
+                .build();
+        Mockito.when(userRepository.findById(mockId))
+                .thenReturn(Optional.of(mockUserEntity));
+
+        AysUser mockUser = userEntityToDomainMapper.map(mockUserEntity);
+
+        // Then
+        Optional<AysUser> user = userAdapter.findById(mockId);
+
+        Assertions.assertTrue(user.isPresent());
+        Assertions.assertEquals(mockUser, user.get());
+
+        // Verify
+        Mockito.verify(userRepository, Mockito.times(1))
+                .findById(mockId);
+    }
+
+    @Test
+    void givenValidId_whenUserFoundById_thenReturnOptionalEmpty() {
+
+        // Given
+        String mockId = AysRandomUtil.generateUUID();
+
+        // When
+        Mockito.when(userRepository.findById(mockId))
+                .thenReturn(Optional.empty());
+
+        // Then
+        Optional<AysUser> user = userAdapter.findById(mockId);
+
+        Assertions.assertFalse(user.isPresent());
+
+        // Verify
+        Mockito.verify(userRepository, Mockito.times(1))
+                .findById(mockId);
+    }
 
 }
