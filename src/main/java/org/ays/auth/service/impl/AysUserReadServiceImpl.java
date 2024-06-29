fb10e83421b94d43dedf13982a1993443a060223
Abdullah Çelik <76781045+AbdullahCelikcode@users.noreply.github.com>
2024-06-29 23:24:22 +0300

    AYS-282 | institutionId and related controls added

--- src/main/java/org/ays/auth/service/impl/AysUserReadServiceImpl.java
+++ src/main/java/org/ays/auth/service/impl/AysUserReadServiceImpl.java
@@ -1,29 +1,51 @@
 package org.ays.auth.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.ays.auth.model.AysIdentity;
 import org.ays.auth.model.AysUser;
+import org.ays.auth.model.AysUserFilter;
 import org.ays.auth.model.request.AysUserListRequest;
 import org.ays.auth.port.AysUserReadPort;
 import org.ays.auth.service.AysUserReadService;
 import org.ays.common.model.AysPage;
 import org.ays.common.model.AysPageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AysUserReadServiceImpl implements AysUserReadService {
 
     //todo javadoc and cover with test
 
-    private final AysUserReadPort aysUserReadPort;
+    private final AysUserReadPort userReadPort;
+    private final AysIdentity identity;
 
     @Override
-    public AysPage<AysUser> findAllByInstitutionId(AysUserListRequest listRequest) {
+    public AysPage<AysUser> findAll(AysUserListRequest listRequest) {
 
         final AysPageable aysPageable = listRequest.getPageable();
 
-        return aysUserReadPort.findAll(aysPageable, listRequest.getFilter(), listRequest.getInstitutionId());
-    }
+        Optional.ofNullable(listRequest.getFilter())
+                .ifPresentOrElse(
+                        filter -> {
+                            if (filter.getInstitutionId() == null) {
+                                filter.setInstitutionId(identity.getInstitutionId());
+                            }
+                        },
+                        () -> {
+                            AysUserFilter filter = AysUserFilter.builder()
+                                    .institutionId(identity.getInstitutionId())
+                                    .build();
 
+                            listRequest.setFilter(filter);
+                        }
+                );
+
+        return userReadPort.findAll(aysPageable, listRequest.getFilter());
+    }
 
 }
