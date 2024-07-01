5346fa7e47b1c3279c985f0686daae88eac52cae
MenekseYuncu <baioretto66@gmail.com>
2024-07-01 15:15:20 +0300

    AYS-282 | The User Filter Builder Class has been created

---
+++ src/test/java/org/ays/auth/model/AysUserFilterBuilder.java
@@ -0,0 +1,53 @@
+package org.ays.auth.model;
+
+import org.ays.auth.model.enums.AysUserStatus;
+import org.ays.common.model.AysPhoneNumber;
+import org.ays.common.model.TestDataBuilder;
+import org.ays.common.util.AysRandomUtil;
+
+import java.util.Set;
+
+public class AysUserFilterBuilder extends TestDataBuilder<AysUserFilter> {
+
+    public AysUserFilterBuilder() {
+        super(AysUserFilter.class);
+    }
+
+    public AysUserFilterBuilder withValidValues() {
+        return new AysUserFilterBuilder()
+                .withFirstName("user")
+                .withStatuses(Set.of(AysUserStatus.ACTIVE))
+                .withInstitutionId(AysRandomUtil.generateUUID());
+    }
+
+    public AysUserFilterBuilder withFirstName(String name) {
+        data.setFirstName(name);
+        return this;
+    }
+
+    public AysUserFilterBuilder withLastName(String lastName) {
+        data.setLastName(lastName);
+        return this;
+    }
+
+    public AysUserFilterBuilder withCity(String city) {
+        data.setCity(city);
+        return this;
+    }
+
+    public AysUserFilterBuilder withStatuses(Set<AysUserStatus> statuses) {
+        data.setStatuses(statuses);
+        return this;
+    }
+
+    public AysUserFilterBuilder withPhoneNumber(AysPhoneNumber phoneNumber) {
+        data.setPhoneNumber(phoneNumber);
+        return this;
+    }
+
+    public AysUserFilterBuilder withInstitutionId(String institutionId) {
+        data.setInstitutionId(institutionId);
+        return this;
+    }
+
+}
