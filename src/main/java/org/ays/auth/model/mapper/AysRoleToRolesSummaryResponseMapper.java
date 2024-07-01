e49dc40d47c41c55ead72482813e5a25d887717f
Egehan Asal <egehanasal.dev@gmail.com>
2024-07-01 10:54:57 +0300

    AYS-238 | Mapper Has Been Created

---
+++ src/main/java/org/ays/auth/model/mapper/AysRoleToRolesSummaryResponseMapper.java
@@ -0,0 +1,28 @@
+package org.ays.auth.model.mapper;
+
+import org.ays.auth.model.AysRole;
+import org.ays.auth.model.response.AysRolesSummaryResponse;
+import org.ays.common.model.mapper.BaseMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * {@link AysRoleToRolesSummaryResponseMapper} is an interface that defines the mapping between an {@link AysRole} and an {@link AysRolesSummaryResponse}.
+ * This interface uses the MapStruct annotation @Mapper to generate an implementation of this interface at compile-time.
+ * <p>The class provides a static method {@code initialize()} that returns an instance of the generated mapper implementation.
+ * <p>The interface extends the MapStruct interface {@link BaseMapper}, which defines basic mapping methods.
+ * The interface adds no additional mapping methods, but simply defines the types to be used in the mapping process.
+ */
+@Mapper
+public interface AysRoleToRolesSummaryResponseMapper extends BaseMapper<AysRole, AysRolesSummaryResponse> {
+
+    /**
+     * Initializes the mapper.
+     *
+     * @return the initialized mapper object.
+     */
+    static AysRoleToRolesSummaryResponseMapper initialize() {
+        return Mappers.getMapper(AysRoleToRolesSummaryResponseMapper.class);
+    }
+
+}
