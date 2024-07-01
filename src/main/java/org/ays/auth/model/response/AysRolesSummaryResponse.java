5ef023e27934b1a0275c72fe2b015f320b5c7af8
Egehan Asal <egehanasal.dev@gmail.com>
2024-07-01 09:58:55 +0300

    AYS-239 | Refactor

--- src/main/java/org/ays/auth/model/response/AysRolesSummaryResponse.java
+++ src/main/java/org/ays/auth/model/response/AysRolesSummaryResponse.java
@@ -3,13 +3,6 @@ package org.ays.auth.model.response;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * Data Transfer Object for role responses.
- * <p>
- * The {@link AysRolesSummaryResponse} class encapsulates the information that is sent back
- * to the client as a response for role-related operations.
- * </p>
- */
 @Getter
 @Setter
 public class AysRolesSummaryResponse {
