cb2bd8027f455ab1dd8e35eb6740a3c3c6cdcec9
Egehan Asal <egehanasal.dev@gmail.com>
2024-07-03 14:05:38 +0300

    AYS-239 | Refactor

--- src/test/java/org/ays/auth/controller/AysRoleEndToEndTest.java
+++ src/test/java/org/ays/auth/controller/AysRoleEndToEndTest.java
@@ -51,7 +51,7 @@ class AysRoleEndToEndTest extends AysEndToEndTest {
 
 
     private final AysRoleToRolesSummaryResponseMapper roleToRolesSummaryResponseMapper = AysRoleToRolesSummaryResponseMapper.initialize();
-    private final AysRoleToResponseMapper roleToRoleResponseMapper = AysRoleToResponseMapper.initialize();
+    private final AysRoleToResponseMapper roleToResponseMapper = AysRoleToResponseMapper.initialize();
 
     private static final String BASE_PATH = "/api/v1";
 
@@ -190,7 +190,7 @@ class AysRoleEndToEndTest extends AysEndToEndTest {
         MockHttpServletRequestBuilder mockHttpServletRequestBuilder = AysMockMvcRequestBuilders
                 .get(endpoint, adminToken.getAccessToken());
 
-        AysRoleResponse mockRoleResponse = roleToRoleResponseMapper
+        AysRoleResponse mockRoleResponse = roleToResponseMapper
                 .map(role);
 
         AysResponse<AysRoleResponse> mockResponse = AysResponse
