805c9ae52295aa2e8ffa0a3f62f6dd96de401fa5
agitrubard <demiragitrubar@gmail.com>
2024-07-04 09:40:09 +0300

    AYS-241 | Role Update Endpoint Has Been Created

--- src/main/java/org/ays/auth/controller/AysRoleController.java
+++ src/main/java/org/ays/auth/controller/AysRoleController.java
@@ -8,19 +8,23 @@ import org.ays.auth.model.mapper.AysRoleToRolesResponseMapper;
 import org.ays.auth.model.mapper.AysRoleToRolesSummaryResponseMapper;
 import org.ays.auth.model.request.AysRoleCreateRequest;
 import org.ays.auth.model.request.AysRoleListRequest;
+import org.ays.auth.model.request.AysRoleUpdateRequest;
 import org.ays.auth.model.response.AysRoleResponse;
 import org.ays.auth.model.response.AysRolesResponse;
 import org.ays.auth.model.response.AysRolesSummaryResponse;
 import org.ays.auth.service.AysRoleCreateService;
 import org.ays.auth.service.AysRoleReadService;
+import org.ays.auth.service.AysRoleUpdateService;
 import org.ays.common.model.AysPage;
 import org.ays.common.model.response.AysPageResponse;
 import org.ays.common.model.response.AysResponse;
 import org.hibernate.validator.constraints.UUID;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,12 +32,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
- * REST controller for managing users.
- * This controller provides endpoints for listing users.
+ * REST controller for managing roles.
+ * This controller provides endpoints for listing, creating, updating, and retrieving roles.
  *
  * @see AysRoleReadService
  * @see AysRoleCreateService
+ * @see AysRoleUpdateService
  */
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
@@ -41,11 +47,13 @@ class AysRoleController {
 
     private final AysRoleReadService roleReadService;
     private final AysRoleCreateService roleCreateService;
+    private final AysRoleUpdateService roleUpdateService;
 
 
     private final AysRoleToRolesResponseMapper roleToRolesResponseMapper = AysRoleToRolesResponseMapper.initialize();
     private final AysRoleToRolesSummaryResponseMapper roleToRolesSummaryResponseMapper = AysRoleToRolesSummaryResponseMapper.initialize();
     private final AysRoleToResponseMapper roleToResponseMapper = AysRoleToResponseMapper.initialize();
+
 
     /**
      * GET /roles/summary : Retrieve a summary of all roles.
@@ -55,7 +63,7 @@ class AysRoleController {
      * </p>
      *
      * @return an {@link AysResponse} containing a list of {@link AysRolesSummaryResponse} objects,
-     *         which represent the summary of roles.
+     * which represent the summary of roles.
      */
     @GetMapping("/roles/summary")
     @PreAuthorize("hasAnyAuthority('user:create', 'user:update')")
@@ -126,6 +134,27 @@ class AysRoleController {
     @PreAuthorize("hasAnyAuthority('role:create')")
     public AysResponse<Void> create(@RequestBody @Valid AysRoleCreateRequest createRequest) {
         roleCreateService.create(createRequest);
+        return AysResponse.SUCCESS;
+    }
+
+
+    /**
+     * Update an existing role based on the provided request data.
+     * <p>
+     * This method is mapped to handle HTTP PUT requests to "/role/{id}". It requires
+     * the user to have the 'role:update' authority to access.
+     * </p>
+     *
+     * @param id            The ID of the role to update.
+     * @param updateRequest The request object containing updated data for the role.
+     * @return An {@link AysResponse} indicating the success of the operation.
+     */
+    @PutMapping("/role/{id}")
+    @PreAuthorize("hasAnyAuthority('role:update')")
+    public AysResponse<Void> update(@PathVariable @UUID final String id,
+                                    @RequestBody @Valid final AysRoleUpdateRequest updateRequest) {
+
+        roleUpdateService.update(id, updateRequest);
         return AysResponse.SUCCESS;
     }
 
