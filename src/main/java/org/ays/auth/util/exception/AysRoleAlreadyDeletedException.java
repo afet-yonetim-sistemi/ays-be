3063074855f8e3f5129b869ae29ba2a43d579f15
MenekseYuncu <baioretto66@gmail.com>
2024-07-05 20:01:38 +0300

    AYS-244 | For the role deletion service, AysRoleAlreadyDeletedException has been added.

---
+++ src/main/java/org/ays/auth/util/exception/AysRoleAlreadyDeletedException.java
@@ -0,0 +1,26 @@
+package org.ays.auth.util.exception;
+
+import org.ays.common.util.exception.AysAlreadyException;
+
+import java.io.Serial;
+
+/**
+ * Exception thrown when a role is deleted and attempting to perform an action that requires a deleted role.
+ */
+public final class AysRoleAlreadyDeletedException extends AysAlreadyException {
+
+    /**
+     * Unique serial version ID.
+     */
+    @Serial
+    private static final long serialVersionUID = -7631303999314005771L;
+
+    /**
+     * Constructs a new AysAlreadyException with the specified detail message.
+     *
+     * @param id the id of the deleted user
+     */
+    public AysRoleAlreadyDeletedException(String id) {
+        super("role is already deleted! id:" + id);
+    }
+}
