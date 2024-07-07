7ecc8dab23bcca40fde56d6dddb26f189df67b3a
Egehan Asal <egehanasal.dev@gmail.com>
2024-07-07 19:08:18 +0300

    AYS-192 | Controller Layer Has Been Created

--- src/main/java/org/ays/auth/controller/AysUserController.java
+++ src/main/java/org/ays/auth/controller/AysUserController.java
@@ -3,15 +3,20 @@ package org.ays.auth.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.ays.auth.model.AysUser;
+import org.ays.auth.model.mapper.AysUserToResponseMapper;
 import org.ays.auth.model.mapper.AysUserToUsersResponseMapper;
 import org.ays.auth.model.request.AysUserListRequest;
+import org.ays.auth.model.response.AysUserResponse;
 import org.ays.auth.model.response.AysUsersResponse;
 import org.ays.auth.service.AysUserReadService;
 import org.ays.common.model.AysPage;
 import org.ays.common.model.response.AysPageResponse;
 import org.ays.common.model.response.AysResponse;
+import org.hibernate.validator.constraints.UUID;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +36,7 @@ class AysUserController {
 
 
     private final AysUserToUsersResponseMapper userToUsersResponseMapper = AysUserToUsersResponseMapper.initialize();
+    private final AysUserToResponseMapper userToResponseMapper = AysUserToResponseMapper.initialize();
 
 
     /**
@@ -56,4 +62,25 @@ class AysUserController {
 
         return AysResponse.successOf(pageOfUsersResponse);
     }
+
+
+    /**
+     * GET /user/{id} : Retrieve the details of a user by its ID.
+     * <p>
+     * This endpoint handles the retrieval of a user by its ID. The user must have the 'user:detail'
+     * authority to access this endpoint.
+     * </p>
+     *
+     * @param id The ID of the user to retrieve.
+     * @return An {@link AysResponse} containing the {@link AysUserResponse} if the user is found.
+     */
+
+    @GetMapping("/user/{id}")
+    @PreAuthorize("hasAuthority('user:detail')")
+    public AysResponse<AysUserResponse> findById(@PathVariable @UUID String id) {
+        final AysUser user = userReadService.findById(id);
+        final AysUserResponse userResponse = userToResponseMapper.map(user);
+        return AysResponse.successOf(userResponse);
+    }
+
 }
