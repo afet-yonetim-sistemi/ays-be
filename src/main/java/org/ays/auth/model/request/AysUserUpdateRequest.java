e01cfdba1e61df829449c6f965d4909869490a4e
Egehan Asal <egehanasal.dev@gmail.com>
2024-07-09 00:00:33 +0300

    AYS-310 | `AysUserUpdateRequest` Has Been Created

---
+++ src/main/java/org/ays/auth/model/request/AysUserUpdateRequest.java
@@ -0,0 +1,48 @@
+package org.ays.auth.model.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.ays.common.model.request.AysPhoneNumberRequest;
+import org.ays.common.util.validation.Email;
+import org.ays.common.util.validation.Name;
+import org.hibernate.validator.constraints.UUID;
+
+import java.util.Set;
+
+@Getter
+@Setter
+public class AysUserUpdateRequest {
+
+    @Name
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String firstName;
+
+    @Name
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String lastName;
+
+    @Email
+    @NotBlank
+    @Size(min = 2, max = 255)
+    private String emailAddress;
+
+    @Name
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String city;
+
+    @Valid
+    @NotNull
+    private AysPhoneNumberRequest phoneNumber;
+
+    @NotEmpty
+    private Set<@UUID String> roleIds;
+
+}
