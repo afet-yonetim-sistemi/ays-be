f81acb748f53d8c053443f3ca6ca2bd9402b3603
Egehan Asal <egehanasal.dev@gmail.com>
2024-07-09 17:48:57 +0300

    AYS-310 | Refactor

--- src/main/java/org/ays/auth/controller/AysUserController.java
+++ src/main/java/org/ays/auth/controller/AysUserController.java
@@ -15,7 +15,12 @@ import org.ays.common.model.response.AysResponse;
 import org.hibernate.validator.constraints.UUID;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * REST controller for managing users.
