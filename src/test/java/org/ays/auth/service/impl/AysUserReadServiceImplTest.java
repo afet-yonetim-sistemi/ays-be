29a1ed12695f0f532ce2c49a66b68c795b0604f6
Egehan Asal <egehanasal.dev@gmail.com>
2024-07-10 12:43:32 +0300

    AYS-192 | User Detail Flow Has Been Created  (#340)

--- src/test/java/org/ays/auth/service/impl/AysUserReadServiceImplTest.java
+++ src/test/java/org/ays/auth/service/impl/AysUserReadServiceImplTest.java
@@ -8,16 +8,19 @@ import org.ays.auth.model.AysUserFilter;
 import org.ays.auth.model.request.AysUserListRequest;
 import org.ays.auth.model.request.AysUserListRequestBuilder;
 import org.ays.auth.port.AysUserReadPort;
+import org.ays.auth.util.exception.AysUserNotExistByIdException;
 import org.ays.common.model.AysPage;
 import org.ays.common.model.AysPageBuilder;
 import org.ays.common.model.AysPageable;
 import org.ays.common.util.AysRandomUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.util.List;
+import java.util.Optional;
 
 class AysUserReadServiceImplTest extends AysUnitTest {
 
@@ -103,6 +106,52 @@ class AysUserReadServiceImplTest extends AysUnitTest {
 
         Mockito.verify(identity, Mockito.times(1))
                 .getInstitutionId();
+    }
+
+
+    @Test
+    void givenValidId_whenUserFoundById_thenReturnUser() {
+
+        // Given
+        AysUser mockUser = new AysUserBuilder()
+                .withValidValues()
+                .build();
+        String mockId = mockUser.getId();
+
+        // When
+        Mockito.when(userReadPort.findById(mockId))
+                .thenReturn(Optional.of(mockUser));
+
+        // Then
+        AysUser result = userReadService.findById(mockId);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(mockUser, result);
+
+        // Verify
+        Mockito.verify(userReadPort, Mockito.times(1))
+                .findById(Mockito.anyString());
+    }
+
+    @Test
+    void givenValidId_whenUserNotFoundById_thenThrowAysUserNotExistByIdException() {
+
+        // Given
+        String mockId = AysRandomUtil.generateUUID();
+
+        // When
+        Mockito.when(userReadPort.findById(mockId))
+                .thenReturn(Optional.empty());
+
+        // Then
+        Assertions.assertThrows(
+                AysUserNotExistByIdException.class,
+                () -> userReadService.findById(mockId)
+        );
+
+        // Verify
+        Mockito.verify(userReadPort, Mockito.times(1))
+                .findById(Mockito.anyString());
     }
 
 }
