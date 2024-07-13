6d364dc1b81ec69fe6d10e17e8505819be6ab2b6
Agit Rubar Demir <61833677+agitrubard@users.noreply.github.com>
2024-07-13 16:02:30 +0300

    AYS-309 | Mail Sending Infrastructure Has Been Created End to End (#342)

---
+++ src/main/java/org/ays/common/service/AysMailService.java
@@ -0,0 +1,9 @@
+package org.ays.common.service;
+
+import org.ays.common.model.AysMail;
+
+public interface AysMailService {
+
+    void send(AysMail mail);
+
+}
--- src/test/java/org/ays/AysUnitTest.java
+++ src/test/java/org/ays/AysUnitTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 public abstract class AysUnitTest {
