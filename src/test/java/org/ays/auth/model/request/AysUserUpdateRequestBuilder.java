eefce9dff45366fcf3a7ec8a5cdbe8c987f7edf1
Egehan Asal <egehanasal.dev@gmail.com>
2024-07-13 16:39:39 +0300

    AYS-310 | `AysUserUpdateRequestBuilder` Has Been Created

---
+++ src/test/java/org/ays/auth/model/request/AysUserUpdateRequestBuilder.java
@@ -0,0 +1,55 @@
+package org.ays.auth.model.request;
+
+import org.ays.common.model.AysPhoneNumber;
+import org.ays.common.model.TestDataBuilder;
+
+import java.util.Set;
+import java.util.UUID;
+
+public class AysUserUpdateRequestBuilder extends TestDataBuilder<AysUserUpdateRequest> {
+
+    public AysUserUpdateRequestBuilder() {
+        super(AysUserUpdateRequest.class);
+    }
+
+    public AysUserUpdateRequestBuilder withValidValues() {
+        return this
+                .withRoleIds(Set.of(UUID.randomUUID().toString()));
+    }
+
+    public AysUserUpdateRequestBuilder withRoleIds(Set<String> roleIds) {
+        data.setRoleIds(roleIds);
+        return this;
+    }
+
+    public AysUserUpdateRequestBuilder withFirstName(String firstName) {
+        data.setFirstName(firstName);
+        return this;
+    }
+
+    public AysUserUpdateRequestBuilder withLastName(String lastName) {
+        data.setLastName(lastName);
+        return this;
+    }
+
+    public AysUserUpdateRequestBuilder withEmailAddress(String emailAddress) {
+        data.setEmailAddress(emailAddress);
+        return this;
+    }
+
+    public AysUserUpdateRequestBuilder withPhoneNumber(AysPhoneNumber phoneNumber) {
+        data.setPhoneNumber(phoneNumber);
+        return this;
+    }
+
+    public AysUserUpdateRequestBuilder withCity(String city) {
+        data.setCity(city);
+        return this;
+    }
+
+    public AysUserUpdateRequestBuilder withStatus(String emailAddress) {
+        data.setEmailAddress(emailAddress);
+        return this;
+    }
+
+}
