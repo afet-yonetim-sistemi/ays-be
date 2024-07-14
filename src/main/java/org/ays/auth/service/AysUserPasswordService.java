6b4a37870fa54bc38eb43a5362b3aab5590ab0a8
agitrubard <demiragitrubar@gmail.com>
2024-07-14 14:00:35 +0300

    AYS-318 | Javadoc Has Been Added to `AysUserPasswordService` Class

--- src/main/java/org/ays/auth/service/AysUserPasswordService.java
+++ src/main/java/org/ays/auth/service/AysUserPasswordService.java
@@ -2,8 +2,18 @@ package org.ays.auth.service;
 
 import org.ays.auth.model.request.AysForgotPasswordRequest;
 
+/**
+ * Service interface for handling user password operations.
+ * Implementations of this interface should provide functionality for handling forgotten passwords.
+ */
 public interface AysUserPasswordService {
 
+    /**
+     * Handles the forgot password request by sending an email to the user
+     * with instructions to create a new password.
+     *
+     * @param forgotPasswordRequest the request containing the user's email address.
+     */
     void forgotPassword(AysForgotPasswordRequest forgotPasswordRequest);
 
 }
