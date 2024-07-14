8d77347767ddbb15c44acf80441aae81f1d8f937
agitrubard <demiragitrubar@gmail.com>
2024-07-14 12:02:42 +0300

    AYS-318 | Validations of `emailAddress` has been Fixed in `AysForgotPasswordRequest` Class

--- src/main/java/org/ays/auth/model/request/AysForgotPasswordRequest.java
+++ src/main/java/org/ays/auth/model/request/AysForgotPasswordRequest.java
@@ -1,9 +1,10 @@
 package org.ays.auth.model.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.ays.common.util.validation.Email;
 
 @Getter
 @Setter
@@ -11,6 +12,7 @@ public class AysForgotPasswordRequest {
 
     @Email
     @NotBlank
+    @Size(min = 2, max = 255)
     private String emailAddress;
 
 }
