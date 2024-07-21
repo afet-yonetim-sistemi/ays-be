f82fe48a23672e675b3d53257e104c1bd3db250a
agitrubard <demiragitrubar@gmail.com>
2024-07-21 15:13:00 +0300

    AYS-310 | User Update Service Has Been Refactored and Institution ID of User Has Been Filtered when Finding User Before Updating User

--- src/main/java/org/ays/auth/service/impl/AysUserUpdateServiceImpl.java
+++ src/main/java/org/ays/auth/service/impl/AysUserUpdateServiceImpl.java
@@ -1,6 +1,7 @@
 package org.ays.auth.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.ays.auth.model.AysIdentity;
 import org.ays.auth.model.AysRole;
 import org.ays.auth.model.AysUser;
 import org.ays.auth.model.request.AysUserUpdateRequest;
@@ -9,10 +10,10 @@ import org.ays.auth.port.AysUserReadPort;
 import org.ays.auth.port.AysUserSavePort;
 import org.ays.auth.service.AysUserUpdateService;
 import org.ays.auth.util.exception.AysRolesNotExistException;
+import org.ays.auth.util.exception.AysUserAlreadyExistsByEmailException;
+import org.ays.auth.util.exception.AysUserAlreadyExistsByPhoneNumberException;
 import org.ays.auth.util.exception.AysUserIsNotActiveOrPassiveException;
 import org.ays.auth.util.exception.AysUserNotExistByIdException;
-import org.ays.auth.util.exception.AysUserAlreadyExistsByPhoneNumberException;
-import org.ays.auth.util.exception.AysUserAlreadyExistsByEmailException;
 import org.ays.common.model.AysPhoneNumber;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,13 +34,15 @@ class AysUserUpdateServiceImpl implements AysUserUpdateService {
     private final AysUserSavePort userSavePort;
     private final AysRoleReadPort roleReadPort;
 
+    private final AysIdentity identity;
+
 
     /**
      * Updates an existing user with the given ID based on the provided update request.
      *
-     * @param id The unique identifier of the user to be updated.
+     * @param id            The unique identifier of the user to be updated.
      * @param updateRequest The request object containing the updated user information.
-     * @throws AysUserNotExistByIdException  if a user with the given ID does not exist.
+     * @throws AysUserNotExistByIdException         if a user with the given ID does not exist.
      * @throws AysUserIsNotActiveOrPassiveException if the user's status is not valid for an update.
      */
     @Override
@@ -47,6 +50,7 @@ class AysUserUpdateServiceImpl implements AysUserUpdateService {
                        final AysUserUpdateRequest updateRequest) {
 
         final AysUser user = userReadPort.findById(id)
+                .filter(userFromDatabase -> identity.getInstitutionId().equals(userFromDatabase.getInstitution().getId()))
                 .orElseThrow(() -> new AysUserNotExistByIdException(id));
 
         if (!(user.isActive() || user.isPassive())) {
@@ -58,22 +62,15 @@ class AysUserUpdateServiceImpl implements AysUserUpdateService {
                 .lineNumber(updateRequest.getPhoneNumber().getLineNumber())
                 .build();
 
-        if (user.getPhoneNumber() != phoneNumber) {
-            this.validatePhoneNumber(id, phoneNumber);
-        }
-
-        if (!(user.getEmailAddress().equals(updateRequest.getEmailAddress()))) {
-            this.validateEmailAddress(id, updateRequest.getEmailAddress());
-        }
-
-        final List<AysRole> roles = this.checkExistingRolesAndGet(updateRequest.getRoleIds());
+        this.validatePhoneNumber(user, phoneNumber);
+        this.validateEmailAddress(user, updateRequest.getEmailAddress());
+        this.validateRolesAndSet(user, updateRequest.getRoleIds());
 
         user.setFirstName(updateRequest.getFirstName());
         user.setLastName(updateRequest.getLastName());
         user.setEmailAddress(updateRequest.getEmailAddress());
         user.setCity(updateRequest.getCity());
         user.setPhoneNumber(phoneNumber);
-        user.setRoles(roles);
 
         userSavePort.save(user);
     }
@@ -83,13 +80,18 @@ class AysUserUpdateServiceImpl implements AysUserUpdateService {
      * Validates the uniqueness of the provided phone number.
      * Checks if there is any existing user with the same phone number.
      *
-     * @param id          The unique identifier of the user being updated.
+     * @param user        The user being updated.
      * @param phoneNumber The phone number to be validated.
      * @throws AysUserAlreadyExistsByPhoneNumberException if the phone number is already associated with another user.
      */
-    private void validatePhoneNumber(String id, AysPhoneNumber phoneNumber) {
+    private void validatePhoneNumber(AysUser user, AysPhoneNumber phoneNumber) {
+
+        if (user.getPhoneNumber().equals(phoneNumber)) {
+            return;
+        }
+
         userReadPort.findByPhoneNumber(phoneNumber)
-                .filter(existingUser -> !existingUser.getId().equals(id))
+                .filter(existingUser -> !existingUser.getId().equals(user.getId()))
                 .ifPresent(existingUser -> {
                     throw new AysUserAlreadyExistsByPhoneNumberException(phoneNumber);
                 });
@@ -100,13 +102,18 @@ class AysUserUpdateServiceImpl implements AysUserUpdateService {
      * Validates the uniqueness of the provided email address.
      * Checks if there is any existing user with the same email address.
      *
-     * @param id           The unique identifier of the user being updated.
+     * @param user         The user being updated.
      * @param emailAddress The email address to be validated.
      * @throws AysUserAlreadyExistsByEmailException if the email address is already associated with another user.
      */
-    private void validateEmailAddress(String id, String emailAddress) {
+    private void validateEmailAddress(AysUser user, String emailAddress) {
+
+        if (user.getEmailAddress().equals(emailAddress)) {
+            return;
+        }
+
         userReadPort.findByEmailAddress(emailAddress)
-                .filter(existingUser -> !existingUser.getId().equals(id))
+                .filter(existingUser -> !existingUser.getId().equals(user.getId()))
                 .ifPresent(existingUser -> {
                     throw new AysUserAlreadyExistsByEmailException(emailAddress);
                 });
@@ -116,15 +123,23 @@ class AysUserUpdateServiceImpl implements AysUserUpdateService {
     /**
      * Checks the existence of roles by their IDs and returns the corresponding role entities.
      *
+     * @param user    The user being updated.
      * @param roleIds The set of role IDs to be checked and retrieved.
-     * @return A list of {@link AysRole} objects corresponding to the provided role IDs.
      * @throws AysRolesNotExistException if any of the provided role IDs do not exist.
      */
-    private List<AysRole> checkExistingRolesAndGet(final Set<String> roleIds) {
+    private void validateRolesAndSet(final AysUser user, final Set<String> roleIds) {
+
+        boolean isRoleNotChanged = user.getRoles().stream()
+                .allMatch(role -> roleIds.contains(role.getId()));
+        if (isRoleNotChanged) {
+            return;
+        }
+
         final List<AysRole> roles = roleReadPort.findAllByIds(roleIds);
 
         if (roles.size() == roleIds.size()) {
-            return roles;
+            user.setRoles(roles);
+            return;
         }
 
         final List<String> notExistsRoleIds = roleIds.stream()
