b2875545930fa44fa82de1b5e1d260e59d45d383
agitrubard <demiragitrubar@gmail.com>
2024-07-24 12:24:23 +0300

    AYS-329 | End to End Test Has Been Fixed for Check Password Changing Validity Flow

--- src/test/java/org/ays/auth/controller/AysAuthEndToEndTest.java
+++ src/test/java/org/ays/auth/controller/AysAuthEndToEndTest.java
@@ -1,6 +1,9 @@
 package org.ays.auth.controller;
 
 import org.ays.AysEndToEndTest;
+import org.ays.auth.model.AysRole;
+import org.ays.auth.model.AysUser;
+import org.ays.auth.model.AysUserBuilder;
 import org.ays.auth.model.enums.AysSourcePage;
 import org.ays.auth.model.request.AysForgotPasswordRequest;
 import org.ays.auth.model.request.AysForgotPasswordRequestBuilder;
@@ -10,17 +13,31 @@ import org.ays.auth.model.request.AysTokenInvalidateRequest;
 import org.ays.auth.model.request.AysTokenRefreshRequest;
 import org.ays.auth.model.response.AysTokenResponse;
 import org.ays.auth.model.response.AysTokenResponseBuilder;
+import org.ays.auth.port.AysRoleReadPort;
+import org.ays.auth.port.AysUserSavePort;
 import org.ays.common.model.response.AysResponse;
 import org.ays.common.model.response.AysResponseBuilder;
+import org.ays.institution.model.Institution;
+import org.ays.institution.model.InstitutionBuilder;
 import org.ays.util.AysMockMvcRequestBuilders;
 import org.ays.util.AysMockResultMatchersBuilders;
 import org.ays.util.AysValidTestData;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.List;
+
 class AysAuthEndToEndTest extends AysEndToEndTest {
+
+    @Autowired
+    private AysUserSavePort userSavePort;
+
+    @Autowired
+    private AysRoleReadPort roleReadPort;
+
 
     private static final String BASE_PATH = "/api/v1/authentication";
 
@@ -130,8 +147,34 @@ class AysAuthEndToEndTest extends AysEndToEndTest {
 
     @Test
     void givenValidId_whenCheckPasswordIdSuccessfully_thenReturnSuccessResponse() throws Exception {
+
+        // Initialize
+        Institution institution = new InstitutionBuilder()
+                .withId(AysValidTestData.Admin.INSTITUTION_ID)
+                .build();
+
+        AysRole role = roleReadPort.findAllActivesByInstitutionId(institution.getId())
+                .stream()
+                .findFirst()
+                .orElseThrow();
+
+        AysUser.Password password = new AysUserBuilder.PasswordBuilder()
+                .withoutId()
+                .withValue("ba9c1156-05d6-410e-a3af-fe6a36935430")
+                .build();
+
+        AysUser user = userSavePort.save(
+                new AysUserBuilder()
+                        .withValidValues()
+                        .withoutId()
+                        .withRoles(List.of(role))
+                        .withInstitution(institution)
+                        .withPassword(password)
+                        .build()
+        );
+
         // Given
-        String mockId = "40fb7a46-40bd-46cb-b44f-1f47162133b1";
+        String mockId = user.getPassword().getId();
 
         // Then
         String endpoint = BASE_PATH.concat("/password/").concat(mockId);
