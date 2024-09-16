2a455bb7d13649982328e5cdb96559a6e8937256
agitrubard <demiragitrubar@gmail.com>
2024-09-16 15:33:50 +0300

    AYS-467 | `@CheckEmergencyEvacuationApplicationActivity` Annotation and Aspect Have Been Created with Aspect Oriented Programming

---
+++ src/main/java/org/ays/emergency_application/util/annotation/CheckEmergencyEvacuationApplicationActivity.java
@@ -0,0 +1,11 @@
+package org.ays.emergency_application.util.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CheckEmergencyEvacuationApplicationActivity {
+}
