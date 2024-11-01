9911f01cf5040dad2d22242c26378f8f92aa9668
Egehan Asal <egehanasal.dev@gmail.com>
2024-11-01 15:06:46 +0300

    AYS-455 | `@UtilityClass` Annotation Has Been Added

--- src/test/java/org/ays/util/UUIDTestUtil.java
+++ src/test/java/org/ays/util/UUIDTestUtil.java
@@ -1,7 +1,10 @@
 package org.ays.util;
 
+import lombok.experimental.UtilityClass;
+
 import java.util.regex.Pattern;
 
+@UtilityClass
 public class UUIDTestUtil {
 
     private static final Pattern UUID_REGEX = Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
