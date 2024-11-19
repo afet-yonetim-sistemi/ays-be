499a8c5ecd636ac056a90822da35ae834396b188
Egehan Asal <egehanasal.dev@gmail.com>
2024-11-19 22:32:36 +0300

    AYS-550 | Tests Have Been Fixed

--- src/test/java/org/ays/auth/controller/AdminRegistrationApplicationControllerTest.java
+++ src/test/java/org/ays/auth/controller/AdminRegistrationApplicationControllerTest.java
@@ -195,7 +195,6 @@ class AdminRegistrationApplicationControllerTest extends AysRestControllerTest {
             "This is a valid text with sufficient length and contains alphabetic characters.",
             "This text includes numbers 12345 and still should be accepted because it's within limits.",
             "This text, which includes punctuation marks, should be accepted.",
-            " This text has leading and trailing spaces which should be trimmed and accepted. ",
             "ÇalıŞkan ve dÜrüst İnsanlar her zaman başarıyı yakalar.(:;?/)"
     })
     void givenValidAdminRegisterApplicationCreateRequest_whenCreatingAdminRegisterApplication_thenReturnAdminRegisterApplicationCreateResponse(String reason) throws Exception {
