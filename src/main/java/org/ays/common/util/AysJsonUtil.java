3be337b742a52bb48f71a0879128c3758c2e1f15
agitrubard <demiragitrubar@gmail.com>
2024-11-27 11:39:08 +0300

    AYS-509 | `toUnformattedJson()` Method Has Been Added to `AysJsonUtil` Class

--- src/main/java/org/ays/common/util/AysJsonUtil.java
+++ src/main/java/org/ays/common/util/AysJsonUtil.java
@@ -1,5 +1,8 @@
 package org.ays.common.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import lombok.experimental.UtilityClass;
@@ -11,6 +14,11 @@ import lombok.experimental.UtilityClass;
 public class AysJsonUtil {
 
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    static {
+        OBJECT_MAPPER.registerModule(new JavaTimeModule());
+    }
 
     /**
      * Converts an object to a JSON string.
@@ -21,4 +29,23 @@ public class AysJsonUtil {
     public static String toJson(Object object) {
         return GSON.toJson(object);
     }
+
+    /**
+     * Converts a formatted JSON string into an unformatted (compact) JSON string.
+     * <p>
+     * This method parses the input JSON string into a tree structure using the {@link ObjectMapper}
+     * and then converts it back to a compact string representation.
+     * If an exception occurs during parsing, an empty string is returned.
+     *
+     * @param json the formatted JSON string to be converted
+     * @return a compact (unformatted) JSON string, or an empty string if the input is invalid
+     */
+    public static String toUnformattedJson(String json) {
+        try {
+            return OBJECT_MAPPER.readTree(json).toString();
+        } catch (JsonProcessingException exception) {
+            return "";
+        }
+    }
+
 }
