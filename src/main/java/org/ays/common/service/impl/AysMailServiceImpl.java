b7459ef68b71487b140beba501a18f0829114def
agitrubard <demiragitrubar@gmail.com>
2024-12-22 13:28:14 +0300

    AYS-623 | Ignored Domains Have Been Added to Mail Service for Decreasing Bounce Limit of Amazon SES

--- src/main/java/org/ays/common/service/impl/AysMailServiceImpl.java
+++ src/main/java/org/ays/common/service/impl/AysMailServiceImpl.java
@@ -17,6 +17,7 @@ import org.springframework.web.context.request.async.AsyncRequestTimeoutExceptio
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
@@ -28,8 +29,22 @@ class AysMailServiceImpl implements AysMailService {
 
     private final JavaMailSender mailSender;
 
+
+    private static final List<String> IGNORED_DOMAINS = List.of(
+            "@afetyonetimsistemi.test"
+    );
+
+
     @Override
     public void send(final AysMail mail) {
+
+        for (String to : mail.getTo()) {
+            boolean ignoredMailExists = IGNORED_DOMAINS.stream().anyMatch(to::endsWith);
+            if (ignoredMailExists) {
+                log.warn("Mail sending is ignored for {} with {} template", to, mail.getTemplate());
+                return;
+            }
+        }
 
         CompletableFuture.runAsync(() -> this.sendEmail(mail))
                 .orTimeout(5, TimeUnit.SECONDS)
