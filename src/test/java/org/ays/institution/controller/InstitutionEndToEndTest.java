7de315b0a28a632ad40fe6970f60a0a0779166e3
İrfan Evrens <irfanevrens@gmail.com>
2025-11-07 20:51:02 +0300

    AYS-967 | Added institution summary endpoint for Landing page

--- src/test/java/org/ays/institution/controller/InstitutionEndToEndTest.java
+++ src/test/java/org/ays/institution/controller/InstitutionEndToEndTest.java
@@ -20,7 +20,8 @@ class InstitutionEndToEndTest extends AysEndToEndTest {
     private final InstitutionToInstitutionsSummaryResponseMapper institutionToInstitutionsSummaryResponseMapper = InstitutionToInstitutionsSummaryResponseMapper.initialize();
 
 
-    private static final String BASE_PATH = "/api/institution/v1";
+    private static final String INSTITUTION_BASE_PATH = "/api/institution/v1";
+    private static final String LANDING_BASE_PATH = "/api/landing/v1";
 
 
     @Test
@@ -33,9 +34,35 @@ class InstitutionEndToEndTest extends AysEndToEndTest {
         );
 
         // Then
-        String endpoint = BASE_PATH.concat("/institutions/summary");
+        String endpoint = INSTITUTION_BASE_PATH.concat("/institutions/summary");
         MockHttpServletRequestBuilder mockHttpServletRequestBuilder = AysMockMvcRequestBuilders
                 .get(endpoint, superAdminToken.getAccessToken());
+
+        List<InstitutionsSummaryResponse> mockInstitutionResponses = institutionToInstitutionsSummaryResponseMapper
+                .map(mockActiveInstitutions);
+        AysResponse<List<InstitutionsSummaryResponse>> mockResponse = AysResponse
+                .successOf(mockInstitutionResponses);
+
+        aysMockMvc.perform(mockHttpServletRequestBuilder, mockResponse)
+                .andExpect(AysMockResultMatchersBuilders.status()
+                        .isOk())
+                .andExpect(AysMockResultMatchersBuilders.response()
+                        .isNotEmpty());
+    }
+
+    @Test
+    void whenActiveInstitutionsExist_thenReturnInstitutionSummaryResponsesForLanding() throws Exception {
+
+        // When
+        List<Institution> mockActiveInstitutions = List.of(
+                new InstitutionBuilder().withValidValues().withStatus(InstitutionStatus.ACTIVE).build(),
+                new InstitutionBuilder().withValidValues().withStatus(InstitutionStatus.ACTIVE).build()
+        );
+
+        // Then
+        String endpoint = LANDING_BASE_PATH.concat("/institutions/summary");
+        MockHttpServletRequestBuilder mockHttpServletRequestBuilder = AysMockMvcRequestBuilders
+                .get(endpoint);
 
         List<InstitutionsSummaryResponse> mockInstitutionResponses = institutionToInstitutionsSummaryResponseMapper
                 .map(mockActiveInstitutions);
