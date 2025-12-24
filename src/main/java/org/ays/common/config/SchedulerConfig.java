0cb0bb97b52c6934ed5afe17fb25c4e3e49bbd11
sinanartun <26043673+sinanartun@users.noreply.github.com>
2025-12-24 18:15:29 +0000

    up

---
+++ src/main/java/org/ays/common/config/SchedulerConfig.java
@@ -0,0 +1,18 @@
+package org.ays.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulerConfig {
+
+    @Bean("aysHeartbeatTaskScheduler")
+    public TaskScheduler aysHeartbeatTaskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(1);
+        taskScheduler.setThreadNamePrefix("wss-heartbeat-thread-");
+        return taskScheduler;
+    }
+}
--- src/main/java/org/ays/common/config/WebSocketConfig.java
+++ src/main/java/org/ays/common/config/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package org.ays.common.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -10,11 +12,17 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final TaskScheduler heartbeatScheduler;
+
+    public WebSocketConfig(@Qualifier("aysHeartbeatTaskScheduler") TaskScheduler heartbeatScheduler) {
+        this.heartbeatScheduler = heartbeatScheduler;
+    }
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic")
-                .setHeartbeatValue(new long[] { 1000, 1000 })
-                .setTaskScheduler(new org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler());
+                .setHeartbeatValue(new long[]{ 10000, 10000 })
+                .setTaskScheduler(heartbeatScheduler);
         config.setApplicationDestinationPrefixes("/app");
     }
 
