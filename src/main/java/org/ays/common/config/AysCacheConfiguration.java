9e6bafab8b7e6cf7b133a822cdbd5f245d8eaf02
agitrubard <demiragitrubar@gmail.com>
2026-01-29 18:24:32 +0300

    AYS-145 | `AysCacheConfiguration` Has Been Created for Enabling To Cache

---
+++ src/main/java/org/ays/common/config/AysCacheConfiguration.java
@@ -0,0 +1,9 @@
+package org.ays.common.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+class AysCacheConfiguration {
+}
