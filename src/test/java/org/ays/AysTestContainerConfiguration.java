8a3bba7723f37bbca84b11ad318cc7d3f32182e1
agitrubard <demiragitrubar@gmail.com>
2026-01-29 18:56:13 +0300

    AYS-145 | Redis Testcontainers Have Been Integrated

--- src/test/java/org/ays/AysTestContainerConfiguration.java
+++ src/test/java/org/ays/AysTestContainerConfiguration.java
@@ -1,5 +1,6 @@
 package org.ays;
 
+import com.redis.testcontainers.RedisContainer;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
@@ -12,8 +13,11 @@ abstract class AysTestContainerConfiguration extends AysLogConfiguration {
             .withDatabaseName("test")
             .withCommand("--max-connections=1000");
 
+    private static final RedisContainer REDIS_CONTAINER = new RedisContainer("redis:8.4.0");
+
     static {
         MYSQL_CONTAINER.start();
+        REDIS_CONTAINER.start();
     }
 
     @DynamicPropertySource
@@ -22,6 +26,8 @@ abstract class AysTestContainerConfiguration extends AysLogConfiguration {
         dynamicPropertyRegistry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
         dynamicPropertyRegistry.add("spring.datasource.writer.url", MYSQL_CONTAINER::getJdbcUrl);
         dynamicPropertyRegistry.add("spring.datasource.reader.url", MYSQL_CONTAINER::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+        dynamicPropertyRegistry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379));
     }
 
 }
