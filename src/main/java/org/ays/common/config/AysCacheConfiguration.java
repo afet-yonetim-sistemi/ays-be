9df825595d9f947132f5ec37f9af47f5f0e3d277
agitrubard <demiragitrubar@gmail.com>
2026-01-30 20:58:35 +0300

    AYS-145 | Javadoc Has Been Added to `AysCacheConfiguration` Class

--- src/main/java/org/ays/common/config/AysCacheConfiguration.java
+++ src/main/java/org/ays/common/config/AysCacheConfiguration.java
@@ -3,6 +3,13 @@ package org.ays.common.config;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Enables caching support for the application.
+ * <p>
+ * Declares the caching infrastructure by activating Spring's cache abstraction.
+ * Cache providers, cache managers, and cache usage are configured elsewhere.
+ * </p>
+ */
 @Configuration
 @EnableCaching
 class AysCacheConfiguration {
