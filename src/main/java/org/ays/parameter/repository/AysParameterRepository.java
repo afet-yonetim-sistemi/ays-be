94995c12fd0ffcb41b123501eb6159ce31547afe
agitrubard <demiragitrubar@gmail.com>
2026-03-07 13:39:21 +0300

    AYS-146 | Unused `AysParameterReadPort.findByName()` and Related Methods Have Been Removed and Tests Have Been Refactored

--- src/main/java/org/ays/parameter/repository/AysParameterRepository.java
+++ src/main/java/org/ays/parameter/repository/AysParameterRepository.java
@@ -1,23 +1,10 @@
 package org.ays.parameter.repository;
 
-import org.ays.parameter.model.AysParameter;
 import org.ays.parameter.model.entity.AysParameterEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 /**
  * Repository interface for performing CRUD operations on {@link AysParameterEntity} instances.
- * Provides methods for finding {@link AysParameterEntity} instances by name prefix.
  */
 public interface AysParameterRepository extends JpaRepository<AysParameterEntity, Long> {
-
-    /**
-     * Finds an {@link AysParameterEntity} instances by name.
-     *
-     * @param name the name to search for
-     * @return an {@link Optional} containing the {@link AysParameter} entity if found, otherwise empty
-     */
-    Optional<AysParameterEntity> findByName(String name);
-
 }
