62c458b654043cf8bb5aaf7fecf1c9edef6f4b35
Meryem Aydın <aydnn.meryem@gmail.com>
2026-09-17 10:39:26 +0300

    refactor(role): remove wildcard imports in AysRoleEntity

--- src/main/java/org/ays/auth/model/entity/AysRoleEntity.java
+++ src/main/java/org/ays/auth/model/entity/AysRoleEntity.java
@@ -1,6 +1,18 @@
 package org.ays.auth.model.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
